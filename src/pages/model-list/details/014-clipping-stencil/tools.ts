d495914bf29589d4773a2c9fd01f224cce4861f5
蒋昂 <jiangang@powerpms.com>
2023-01-13 14:07:11 +0800

    深度解析 clipping-stencil 案例

--- src/pages/model-list/details/014-clipping-stencil/tools.ts
+++ src/pages/model-list/details/014-clipping-stencil/tools.ts
@@ -72,7 +72,12 @@ export class Model {
 
   // 初始化方法入口
   init() {
-    // 实例化时钟
+    // 实例化时钟 Clock( autoStart : Boolean )
+    // 该对象用于跟踪时间。如果performance.now可用，
+    // 则 Clock 对象通过该方法实现，否则回落到使用略欠精准的Date.now来实现
+
+    // Clock( autoStart : Boolean )
+    // autoStart — (可选) 是否要在第一次调用 .getDelta() 时自动开启时钟。默认值是 true
     this.clock = new THREE.Clock();
 
     // 实例化相机
@@ -81,6 +86,7 @@ export class Model {
 
     // 创建场景
     this.scene = new THREE.Scene();
+    // 添加环境光
     this.scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
     // 创建直线光
@@ -101,10 +107,17 @@ export class Model {
       (this.scene as THREE.Scene).add(helper);
     });
 
+    // geometry 创建几何体
     this.object = new THREE.Group();
     this.scene.add(this.object);
-
-    // geometry
+    // 圆环缓冲扭结几何体（TorusKnotGeometry）
+    // TorusKnotGeometry(radius : Float, tube : Float, tubularSegments : Integer, radialSegments : Integer, p : Integer, q : Integer)
+    // radius - 圆环的半径，默认值为1
+    // tube — 管道的半径，默认值为0.4
+    // tubularSegments — 管道的分段数量，默认值为64
+    // radialSegments — 横截面分段数量，默认值为8
+    // p — 这个值决定了几何体将绕着其旋转对称轴旋转多少次，默认值是2
+    // q — 这个值决定了几何体将绕着其内部圆环旋转多少次，默认值是3
     const geometry = new THREE.TorusKnotGeometry(0.4, 0.10, 220, 60);
     const planeGeometry =  new THREE.PlaneGeometry(4, 4);
     for (let i = 0; i < 3; i++) {
@@ -145,33 +158,40 @@ export class Model {
       shadowSide: THREE.DoubleSide,
     });
 
-    // add the color
+    //添加颜色
     const clippedColorFront = new THREE.Mesh(geometry, material);
     clippedColorFront.castShadow = true;
     clippedColorFront.renderOrder = 6;
     this.object.add(clippedColorFront);
 
+    // 添加投影板
     const ground = new THREE.Mesh(
       new THREE.PlaneGeometry(9, 9, 1, 1),
       new THREE.ShadowMaterial({color: 0x000000, opacity: 0.25, side: THREE.DoubleSide})
     );
     ground.rotation.x = -Math.PI / 2;
     ground.position.y = -1;
+    // 接收阴影
     ground.receiveShadow = true;
     this.scene.add(ground);
 
-    // Renderer
+    // 渲染器
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.shadowMap.enabled = true;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
+    // .setClearColor ( color : Color, alpha : Float )
+    // 设置渲染器的颜色及其透明度
     this.renderer.setClearColor(0x263238);
+    // 定义渲染器是否考虑对象级剪切平面。 默认为false
     this.renderer.localClippingEnabled = true;
     this.container.appendChild(this.renderer.domElement);
 
     // 控制器
     this.controls = new OrbitControls(this.camera,this.renderer.domElement );
+    // 你能够将相机向内移动多少（仅适用于PerspectiveCamera），其默认值为0
     this.controls.minDistance = 2;
+    // 你能够将相机向外移动多少（仅适用于PerspectiveCamera），其默认值为Infinity
     this.controls.maxDistance = 20;
     this.controls.update();
     
@@ -247,12 +267,19 @@ export class Model {
   private createPlaneStencilGroup(geometry: THREE.TorusKnotGeometry, plane: THREE.Plane, renderOrder: number) {
     const group = new THREE.Group();
 
-    // 基础材质
+    // 基础材质 基础网格材质(MeshBasicMaterial)
+    // 一个以简单着色（平面或线框）方式来绘制几何体的材质。这种材质不受光照的影响。
     const baseMat = new THREE.MeshBasicMaterial();
+    // 渲染此材质是否对深度缓冲区有任何影响。默认为true
+    // 在绘制2D叠加时，将多个事物分层在一起而不创建z-index时，禁用深度写入会很有用
     baseMat.depthWrite = false;
+    // 是否在渲染此材质时启用深度测试。默认为 true
     baseMat.depthTest = false;
+    // 是否渲染材质的颜色。 这可以与网格的renderOrder属性结合使用，以创建遮挡其他对象的不可见对象。默认值为true。
     baseMat.colorWrite = false;
+    // 是否对模板缓冲执行模板操作，如果执行写入或者与模板缓冲进行比较，这个值需要设置为true。默认为false。
     baseMat.stencilWrite = true;
+    // 使用模板比较时所用的方法，默认为AlwaysStencilFunc。在模板函数 constants 中查看可用的值
     baseMat.stencilFunc = THREE.AlwaysStencilFunc;
 
     // back faces
@@ -270,12 +297,18 @@ export class Model {
     // front faces
     const mat1 = baseMat.clone();
     mat1.side = THREE.FrontSide;
+    // 用户定义的剪裁平面，在世界空间中指定为THREE.Plane对象
     mat1.clippingPlanes = [ plane ];
+    // 当比较函数没有通过的时候要执行的模板操作，默认为KeepStencilOp，在模板操作 constants 查看可用值。
     mat1.stencilFail = THREE.DecrementWrapStencilOp;
+    // 当比较函数通过了但是深度检测没有通过的时候要执行的模板操作， 默认为KeepStencilOp，在模板操作 constants 查看可用值
     mat1.stencilZFail = THREE.DecrementWrapStencilOp;
+    // 当比较函数和深度检测都通过时要执行的模板操作，默认为KeepStencilOp，在模板操作constants 中查看可用值
     mat1.stencilZPass = THREE.DecrementWrapStencilOp;
 
     const mesh1 = new THREE.Mesh(geometry, mat1);
+    // 这个值将使得scene graph（场景图）中默认的的渲染顺序被覆盖
+    // 即使不透明对象和透明对象保持独立顺序。 渲染顺序是由低到高来排序的，默认值为0
     mesh1.renderOrder = renderOrder;
     group.add(mesh1);
 
