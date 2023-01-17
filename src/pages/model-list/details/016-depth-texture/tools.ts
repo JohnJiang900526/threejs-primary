b97c28eee0edd4c2c10639407f796e5792026f87
蒋昂 <jiangang@powerpms.com>
2023-01-17 13:31:49 +0800

    深度纹理案例 解析

--- src/pages/model-list/details/016-depth-texture/tools.ts
+++ src/pages/model-list/details/016-depth-texture/tools.ts
@@ -98,14 +98,23 @@ export class Model {
     const message = "Your browser does not support <strong>WEBGL_depth_texture</strong>.<br/><br/>This demo will not work."
     // 创建一个渲染器
     this.renderer = new THREE.WebGLRenderer();
-    this.notSupport = (this.renderer.capabilities.isWebGL2 === false && this.renderer.extensions.has("WEBGL_depth_texture") === false);
-    if (this.notSupport) { 
-      showDialog({ title: "提示", message });
-      return false;
-    }
+    // .setPixelRatio ( value : number ) : undefined
+    // 设置设备像素比。通常用于避免HiDPI设备上绘图模糊
     this.renderer.setPixelRatio(window.devicePixelRatio);
+    // .setSize ( width : Integer, height : Integer, updateStyle : Boolean ) : undefined
+    // 将输出canvas的大小调整为(width, height)并考虑设备像素比，且将视口从(0, 0)开始调整到适合大小 
+    // 将updateStyle设置为false以阻止对canvas的样式做任何改变
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
+    this.notSupport = (
+      this.renderer.capabilities.isWebGL2 === false
+      &&
+      this.renderer.extensions.has("WEBGL_depth_texture") === false
+    );
+    if (this.notSupport) { 
+      showDialog({title: "提示", message});
+      return false;
+    }
 
     // 创建一个相机
     this.camera = new THREE.PerspectiveCamera(70, this.width/this.height, 0.01, 50);
@@ -113,7 +122,13 @@ export class Model {
 
     // 创建一个控制器
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    // .enableDamping : Boolean
+    // 将其设置为true以启用阻尼（惯性），这将给控制器带来重量感。默认值为false
+    // 请注意，如果该值被启用，你将必须在你的动画循环里调用.update()
     this.controls.enableDamping = true;
+    // .autoRotate : Boolean
+    // 将其设为true，以自动围绕目标旋转
+    // 请注意，如果它被启用，你必须在你的动画循环里调用.update()
     this.controls.autoRotate = true;
 
     // 创建深度纹理的渲染目标
@@ -131,24 +146,7 @@ export class Model {
     this.resize();
   }
 
-  // 创建深度纹理的渲染目标
-  private setupRenderTarget() {
-    if (this.target) { this.target.dispose(); }
-
-    // @ts-ignore
-    const format = parseFloat(this.params.format);
-    // @ts-ignore
-    const type = parseFloat(this.params.type);
-
-    this.target = new THREE.WebGLRenderTarget(this.width, this.height);
-    this.target.texture.minFilter = THREE.NearestFilter;
-    this.target.texture.magFilter = THREE.NearestFilter;
-    this.target.stencilBuffer = (format === THREE.DepthStencilFormat) ? true: false;
-    this.target.depthTexture = new THREE.DepthTexture(0, 0);
-    this.target.depthTexture.format = format;
-    this.target.depthTexture.type = type;
-  }
-
+  
   setFormat(val: "DepthFormat" | "DepthStencilFormat") {
     switch(val) {
       case "DepthFormat":
@@ -181,6 +179,39 @@ export class Model {
     this.setupRenderTarget();
   }
 
+  // 创建深度纹理的渲染目标
+  private setupRenderTarget() {
+    if (this.target) { this.target.dispose(); }
+
+    // @ts-ignore
+    const format = parseFloat(this.params.format);
+    // @ts-ignore
+    const type = parseFloat(this.params.type);
+
+    // render target是一个缓冲，就是在这个缓冲中，视频卡为正在后台渲染的场景绘制像素。 
+    // 它用于不同的效果，例如用于在一个图像显示在屏幕上之前先做一些处理
+    // WebGLRenderTarget(width : Number, height : Number, options : Object)
+    // width -renderTarget的宽度. Default is 1.
+    // height - renderTarget的高度. Default is 1.
+    // options - (可选)一个保存着自动生成的目标纹理的纹理参数以及表示是否使用深度缓存/模板缓存的布尔值的对象
+    this.target = new THREE.WebGLRenderTarget(this.width, this.height);
+    // 当一个纹素覆盖小于一个像素时，贴图将如何采样。 其默认值为THREE.NearestFilter
+    this.target.texture.minFilter = THREE.NearestFilter;
+    // 当一个纹素覆盖大于一个像素时，贴图将如何采样。 其默认值为THREE.NearestFilter
+    this.target.texture.magFilter = THREE.NearestFilter;
+    // .stencilBuffer : Boolean
+    // 渲染到模板缓冲区。默认为false
+    this.target.stencilBuffer = (format === THREE.DepthStencilFormat) ? true: false;
+    // .depthTexture : DepthTexture
+    // 如果设置，那么场景的深度将会被渲染到此纹理上。默认为null
+    this.target.depthTexture = new THREE.DepthTexture(5, 5);
+    // 深度纹理（DepthTexture）
+    // format -- 这个值必须是DepthFormat（默认值）或者DepthStencilFormat。 
+    // 请参阅format constants（格式常量）来了解详细信息。
+    this.target.depthTexture.format = format;
+    this.target.depthTexture.type = type;
+  }
+
   // 创建渲染场景
   private setupScene() {
     const count = 50;
@@ -198,6 +229,8 @@ export class Model {
       const zScale = Math.sqrt(1.0 - z * z) * scale;
 
       mesh.position.set(Math.cos(r) * zScale, Math.sin(r) * zScale, z * scale);
+      // .rotation : Euler
+      // 物体的局部旋转，以弧度来表示
       mesh.rotation.set(Math.random(), Math.random(), Math.random());
       this.scene.add(mesh);
     }
@@ -205,9 +238,13 @@ export class Model {
 
   // 设置Post
   private setupPost() {
+    // 正交相机（OrthographicCamera）
     this.postCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    // 着色器材质(ShaderMaterial)
     this.postMaterial = new THREE.ShaderMaterial({
+      // 顶点着色器的GLSL代码。这是shader程序的实际代码
       vertexShader: this.vertexShader.trim(),
+      // 片元着色器的GLSL代码。这是shader程序的实际代码
       fragmentShader: this.fragmentShader.trim(),
       uniforms: {
         cameraNear: { value: (this.camera as THREE.PerspectiveCamera).near },
@@ -218,7 +255,8 @@ export class Model {
     });
 
     const postPlane = new THREE.PlaneGeometry(2, 2);
-    const postQuad = new THREE.Mesh( postPlane, this.postMaterial );
+    const postQuad = new THREE.Mesh(postPlane, this.postMaterial);
+
     this.postScene = new THREE.Scene();
     this.postScene.add(postQuad);
   }
