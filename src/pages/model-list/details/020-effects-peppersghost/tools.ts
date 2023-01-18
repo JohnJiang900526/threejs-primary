0841ba93ee508521270f3f34dad5c41ebbe3d501
蒋昂 <jiangang@powerpms.com>
2023-01-18 13:33:27 +0800

    完成 effects-peppersghost 案例

--- src/pages/model-list/details/020-effects-peppersghost/tools.ts
+++ src/pages/model-list/details/020-effects-peppersghost/tools.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
-import { PeppersGhostEffect } from 'three/examples/jsm/effects/PeppersGhostEffect.js';
+import { PeppersGhostEffect } from 'three/examples/jsm/effects/PeppersGhostEffect';
 
 export class Model {
   private width: number;
@@ -9,8 +9,9 @@ export class Model {
   private scene: null | THREE.Scene;
   private renderer: null | THREE.WebGLRenderer;
   private camera: null | THREE.PerspectiveCamera;
-  private stats: null | Stats;
+  private stats: null | Stats; 
   private effect: null | PeppersGhostEffect
+  private group: null | THREE.Group
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -20,11 +21,59 @@ export class Model {
     this.camera = null;
     this.stats = null;
     this.effect = null;
+    this.group = null;
   }
 
   // 初始化方法入口
   init() {
-    
+    // 创建相机
+    this.camera = new THREE.PerspectiveCamera(100, this.width/this.height, 0.01, 100000);
+
+    // 创建一个场景
+    this.scene = new THREE.Scene();
+
+    // 创建一个分组
+    this.group = new THREE.Group();
+    this.scene.add(this.group);
+
+    // 创建cube立方体
+    const geometry = new THREE.BoxGeometry().toNonIndexed();
+    const position = geometry.attributes.position;
+    const colors = [];
+    const color = new THREE.Color();
+    // 为立方体的每一面生成不同的颜色
+    for (let i = 0; i < position.count; i += 6) {
+      color.setHex(Math.random() * 0xffffff);
+      // 第一面
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
+      // 第二面
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
+    }
+    geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+    // 创建材质
+    const material = new THREE.MeshBasicMaterial({vertexColors: true});
+    for (let i = 0; i < 10; i ++) {
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.x = Math.random() * 2 - 1;
+      cube.position.y = Math.random() * 2 - 1;
+      cube.position.z = Math.random() * 2 - 1;
+      cube.scale.multiplyScalar(Math.random() + 0.5);
+      this.group.add(cube);
+    }
+
+    // 创建渲染器
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.container.appendChild(this.renderer.domElement);
+
+    // 创建effect
+    this.effect = new PeppersGhostEffect(this.renderer);
+    this.effect.setSize(this.width, this.height);
+    this.effect.cameraDistance = 5;
 
     // 初始化 性能统计
     this.initStats();
@@ -33,8 +82,6 @@ export class Model {
     // 窗口自适应
     this.resize();
   }
-
-
 
   // 性能统计
   private initStats() {
@@ -52,6 +99,11 @@ export class Model {
 
     // 统计信息更新
     if (this.stats) { this.stats.update(); }
+
+    // group
+    if (this.group) {
+      this.group.rotation.y += 0.01;
+    }
 
     // 执行渲染
     if (this.scene && this.camera && this.effect) {
