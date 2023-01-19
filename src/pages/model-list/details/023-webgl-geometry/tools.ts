28b522cbc88de3b1acf42adf09b00a7b9e634d17
蒋昂 <jiangang@powerpms.com>
2023-01-19 19:36:32 +0800

    几何体 代码优化

--- src/pages/model-list/details/023-webgl-geometry/tools.ts
+++ src/pages/model-list/details/023-webgl-geometry/tools.ts
@@ -12,6 +12,8 @@ export class Model {
   private camera: null | THREE.PerspectiveCamera;
   private stats: null | Stats;
   private object: null | THREE.Mesh
+  private texture: null | THREE.Texture
+  private material: null | THREE.MeshPhongMaterial
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -22,6 +24,8 @@ export class Model {
     this.stats = null;
     this.object = null;
     this.controls = null;
+    this.texture = null;
+    this.material = null;
   }
 
   // 初始化方法入口
@@ -41,81 +45,28 @@ export class Model {
     this.scene.add(this.camera);
 
     // 加载贴图
-    const texture = new THREE.TextureLoader().load("/examples/textures/uv_grid_opengl.jpg");
+    this.texture = new THREE.TextureLoader().load("/examples/textures/uv_grid_opengl.jpg");
     // .wrapS : number
     // 这个值定义了纹理贴图在水平方向上将如何包裹，在UV映射中对应于U
     // 默认值是THREE.ClampToEdgeWrapping，即纹理边缘将被推到外部边缘的纹素
     // 其它的两个选项分别是THREE.RepeatWrapping和THREE.MirroredRepeatWrapping
-    texture.wrapS = THREE.RepeatWrapping;
+    this.texture.wrapS = THREE.RepeatWrapping;
     // .wrapT : number
     // 这个值定义了纹理贴图在垂直方向上将如何包裹，在UV映射中对应于V。
     // 可以使用与 .wrapS : number相同的选项。
-    texture.wrapT = THREE.RepeatWrapping;
+    this.texture.wrapT = THREE.RepeatWrapping;
     // .anisotropy : number
     // 沿着轴，通过具有最高纹素密度的像素的样本数。 默认情况下，这个值为1
     // 设置一个较高的值将会产生比基本的mipmap更清晰的效果，代价是需要使用更多纹理样本
-    texture.anisotropy = 16;
+    this.texture.anisotropy = 16;
 
     // 创建材质
-    const material = new THREE.MeshPhongMaterial({map: texture, side: THREE.DoubleSide});
+    this.material = new THREE.MeshPhongMaterial({map: this.texture, side: THREE.DoubleSide});
 
-    // 第一排
-    this.object = new THREE.Mesh(new THREE.SphereGeometry(75, 20, 10), material);
-    this.object.position.set(-300, 0, 200);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.IcosahedronGeometry(75, 1), material);
-    this.object.position.set(-100, 0, 200);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.OctahedronGeometry(75, 2), material);
-    this.object.position.set(100, 0, 200);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.TetrahedronGeometry(75, 0), material);
-    this.object.position.set(300, 0, 200);
-    this.scene.add(this.object);
-
-
-    // 第二排
-    this.object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 4, 4), material);
-    this.object.position.set(-300, 0, 0);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100, 4, 4, 4), material);
-    this.object.position.set(-100, 0, 0);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.CircleGeometry(50, 20, 0, Math.PI * 2), material);
-    this.object.position.set(100, 0, 0);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.RingGeometry(10, 50, 20, 5, 0, Math.PI * 2), material);
-    this.object.position.set(300, 0, 0);
-    this.scene.add(this.object);
-
-    // 第三排
-    this.object = new THREE.Mesh(new THREE.CylinderGeometry( 25, 75, 100, 40, 5 ), material);
-    this.object.position.set(-300, 0, - 200);
-    this.scene.add(this.object);
-
-    const points = [];
-    for ( let i = 0; i < 50; i++ ) {
-      const point = new THREE.Vector2(Math.sin(i * 0.2) * Math.sin(i * 0.1) * 15 + 50, (i - 5 ) * 2);
-      points.push(point);
-    }
-
-    this.object = new THREE.Mesh(new THREE.LatheGeometry(points, 20), material);
-    this.object.position.set(-100, 0, -200);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.TorusGeometry(50, 20, 20, 20), material);
-    this.object.position.set(100, 0, -200);
-    this.scene.add(this.object);
-
-    this.object = new THREE.Mesh(new THREE.TorusKnotGeometry(50, 10, 50, 20), material);
-    this.object.position.set(300, 0, -200);
-    this.scene.add(this.object);
+    // 创建几何体
+    this.createFirstRow();
+    this.createSecondtRow();
+    this.createThirdRow();
 
     // 渲染器
     this.renderer = new THREE.WebGLRenderer({antialias: true});
@@ -139,6 +90,69 @@ export class Model {
     this.animate();
     // 窗口自适应
     this.resize();
+  }
+
+  // 创建第1排几何体
+  createFirstRow() {
+    this.object = new THREE.Mesh(new THREE.SphereGeometry(75, 20, 10), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-300, 0, 200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.IcosahedronGeometry(75, 1), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-100, 0, 200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.OctahedronGeometry(75, 2), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(100, 0, 200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.TetrahedronGeometry(75, 0), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(300, 0, 200);
+    (this.scene as THREE.Scene).add(this.object);
+  }
+
+  // 创建第2排几何体
+  createSecondtRow() {
+    this.object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 4, 4), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-300, 0, 0);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100, 4, 4, 4), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-100, 0, 0);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.CircleGeometry(50, 20, 0, Math.PI * 2), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(100, 0, 0);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.RingGeometry(10, 50, 20, 5, 0, Math.PI * 2), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(300, 0, 0);
+    (this.scene as THREE.Scene).add(this.object);
+  }
+
+  // 创建第3排几何体
+  createThirdRow() {
+    this.object = new THREE.Mesh(new THREE.CylinderGeometry( 25, 75, 100, 40, 5 ), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-300, 0, - 200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    const points = [];
+    for ( let i = 0; i < 50; i++ ) {
+      const point = new THREE.Vector2(Math.sin(i * 0.2) * Math.sin(i * 0.1) * 15 + 50, (i - 5 ) * 2);
+      points.push(point);
+    }
+
+    this.object = new THREE.Mesh(new THREE.LatheGeometry(points, 20), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(-100, 0, -200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.TorusGeometry(50, 20, 20, 20), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(100, 0, -200);
+    (this.scene as THREE.Scene).add(this.object);
+
+    this.object = new THREE.Mesh(new THREE.TorusKnotGeometry(50, 10, 50, 20), this.material as THREE.MeshPhongMaterial);
+    this.object.position.set(300, 0, -200);
+    (this.scene as THREE.Scene).add(this.object);
   }
 
   isMobile() {
