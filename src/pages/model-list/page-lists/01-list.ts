877fa9c67475d69533c99a55398f71473748c79e
蒋昂 <jiangang@powerpms.com>
2023-01-31 13:20:49 +0800

    调整 项目目录

---
+++ src/pages/model-list/page-lists/01-list.ts
@@ -0,0 +1,170 @@
+
+// 列表接口类型
+export interface listType {
+  key: string,
+  title: string,
+  path: string,
+  [key: string]: any
+}
+
+// 列表数据
+const list01: listType[] = [
+  {
+    key: "030",
+    title: "030.WEBGL Geometry挤压形状",
+    path: "/list/geometry-extrude-shape"
+  },
+  {
+    key: "029",
+    title: "029.WEBGL Geometry动态",
+    path: "/list/geometry-dynamic"
+  },
+  {
+    key: "028",
+    title: "028.WEBGL Geometry立方体",
+    path: "/list/geometry-cube"
+  },
+  {
+    key: "027",
+    title: "027.WEBGL Geometry凸多边形",
+    path: "/list/geometry-convex"
+  },
+  {
+    key: "026",
+    title: "026.WEBGL Geometry颜色查询",
+    path: "/list/geometry-color-lookup"
+  },
+  {
+    key: "025",
+    title: "025.WEBGL Geometry颜色",
+    path: "/list/geometry-colors"
+  },
+  {
+    key: "024",
+    title: "024.WEBGL Geometry参数",
+    path: "/list/geometries-parametric"
+  },
+  {
+    key: "023",
+    title: "023.WEBGL Geometry几何体",
+    path: "/list/webgl-geometry"
+  },
+  {
+    key: "022",
+    title: "022.WEBGL FrameBuffer纹理",
+    path: "/list/framebuffer-texture"
+  },
+  {
+    key: "021",
+    title: "021.WEBGL 立体效果",
+    path: "/list/effects-stereo"
+  },
+  {
+    key: "020",
+    title: "020.WEBGL 辣椒鬼效应(佩珀幽灵)",
+    path: "/list/effects-peppersghost"
+  },
+  {
+    key: "019",
+    title: "019.WEBGL 影响视差",
+    path: "/list/effects-parallaxbarrier"
+  },
+  {
+    key: "018",
+    title: "018.WEBGL Ascii效果",
+    path: "/list/effects-ascii"
+  },
+  {
+    key: "017",
+    title: "017.WEBGL补色效果",
+    path: "/list/effects-anaglyph"
+  },
+  {
+    key: "016",
+    title: "016.WEBGL深度纹理",
+    path: "/list/depth-texture"
+  },
+  {
+    key: "015",
+    title: "015.WEBGL贴花印图案",
+    path: "/list/webgl-decals"
+  },
+  {
+    key: "014",
+    title: "014.WEBGL剪裁模板",
+    path: "/list/clipping-stencil"
+  },
+  {
+    key: "013",
+    title: "013.WEBGL裁剪交点",
+    path: "/list/clipping-intersection"
+  },
+  {
+    key: "012",
+    title: "012.WEBGL高级剪裁",
+    path: "/list/clipping-advanced"
+  },
+  {
+    key: "011",
+    title: "011.WEBGL裁剪",
+    path: "/list/webgl-clipping"
+  },
+  {
+    key: "010",
+    title: "010.WEBGL相机对数微深度缓冲",
+    path: "/list/logarithmic-depth-buffer"
+  },
+  {
+    key: "009",
+    title: "009.WEBGL相机电影",
+    path: "/list/webgl-camera-cinematic"
+  },
+  {
+    key: "008",
+    title: "008.WEBGL相机数组",
+    path: "/list/webgl-camera-array"
+  },
+  {
+    key: "007",
+    title: "007.WEBGL相机",
+    path: "/list/webgl-camera"
+  },
+  {
+    key: "006",
+    title: "006.多个机器人",
+    path: "/list/animation-multiple"
+  },
+  {
+    key: "005",
+    title: "005.卡通机器人变换",
+    path: "/list/skinning-morph"
+  },
+  {
+    key: "004",
+    title: "004.卡通机器人动作变换",
+    path: "/list/skinning-ik"
+  },
+  {
+    key: "003",
+    title: "003.机器人动画模型2",
+    path: "/list/skinning-additive-blending"
+  },
+  {
+    key: "002",
+    title: "002.机器人动画模型1",
+    path: "/list/skinning-blending"
+  },
+  {
+    key: "001",
+    title: "001.小房子模型",
+    path: "/list/key-frame"
+  },
+  {
+    key: "000",
+    title: "000.矩形阵列",
+    path: "/list/demo"
+  },
+];
+
+export default list01;
+
---
+++ src/router/router-lists/01-list.ts
@@ -0,0 +1,224 @@
+import type { RouteRecordRaw } from "vue-router";
+
+import Demo from "@/pages/model-list/detail01/000-demo/index.vue";
+import KeyFrame from "@/pages/model-list/detail01/001-keyframe/index.vue";
+import SkinningBlending from "@/pages/model-list/detail01/002-skinning-blending/index.vue";
+import SkinningAdditiveBlending from "@/pages/model-list/detail01/003-skinning-additive-blending/index.vue";
+import SkinningIk from "@/pages/model-list/detail01/004-skinning_ik/index.vue";
+import SkinningMorph from "@/pages/model-list/detail01/005-skinning-morph/index.vue";
+import AnimationMultiple from "@/pages/model-list/detail01/006-animation-multiple/index.vue";
+import WebglCamera from "@/pages/model-list/detail01/007-webgl-camera/index.vue";
+import WebglCameraArray from  "@/pages/model-list/detail01/008-webgl-camera-array/index.vue";
+import WebglCameraCinematic from "@/pages/model-list/detail01/009-webgl-camera-cinematic/index.vue";
+import LogarithMicdepthBuffer from "@/pages/model-list/detail01/010-logarithmic-depth-buffer/index.vue";
+import WebglClipping from "@/pages/model-list/detail01/011-webgl-clipping/index.vue";
+import ClippingAdvanced from "@/pages/model-list/detail01/012-clipping-advanced/index.vue";
+import ClippingIntersection from "@/pages/model-list/detail01/013-clipping-intersection/index.vue";
+import ClippingStencil from "@/pages/model-list/detail01/014-clipping-stencil/index.vue";
+import WebglDecals from "@/pages/model-list/detail01/015-webgl-decals/index.vue";
+import DepthTexture from "@/pages/model-list/detail01/016-depth-texture/index.vue";
+import EffectsAnaglyph from "@/pages/model-list/detail01/017-effects-anaglyph/index.vue";
+import EffectsAscii from "@/pages/model-list/detail01/018-effects-ascii/index.vue";
+import EffectsParallaxbarrier from "@/pages/model-list/detail01/019-effects-parallaxbarrier/index.vue";
+import EffectsPeppersghost from "@/pages/model-list/detail01/020-effects-peppersghost/index.vue";
+import EffectsStereo from "@/pages/model-list/detail01/021-effects-stereo/index.vue";
+import FramebufferTexture from "@/pages/model-list/detail01/022-framebuffer-texture/index.vue";
+import WebglGeometry from "@/pages/model-list/detail01/023-webgl-geometry/index.vue";
+import GeometriesParametric from "@/pages/model-list/detail01/024-geometries-parametric/index.vue";
+import GeometryColors from "@/pages/model-list/detail01/025-geometry-colors/index.vue";
+import GeometryColorLookup from "@/pages/model-list/detail01/026-geometry-color-lookup/index.vue";
+import GeometryConvex from "@/pages/model-list/detail01/027-geometry-convex/index.vue";
+import GeometryCube from "@/pages/model-list/detail01/028-geometry-cube/index.vue";
+import GeometryDynamic from "@/pages/model-list/detail01/029-geometry-dynamic/index.vue";
+import GeometryExtrudeShape from "@/pages/model-list/detail01/030-geometry-extrude-shape/index.vue";
+
+const routerList01: RouteRecordRaw[] = [
+  {
+    path: '/list/geometry-extrude-shape',
+    name: 'GeometryExtrudeShape',
+    meta: {title: "geometry-extrude-shape"},
+    component: GeometryExtrudeShape,
+  },
+  {
+    path: '/list/geometry-dynamic',
+    name: 'GeometryDynamic',
+    meta: {title: "geometry-dynamic"},
+    component: GeometryDynamic,
+  },
+  {
+    path: '/list/geometry-cube',
+    name: 'GeometryCube',
+    meta: {title: "geometry-cube"},
+    component: GeometryCube,
+  },
+  {
+    path: '/list/geometry-convex',
+    name: 'GeometryConvex',
+    meta: {title: "geometry-convex"},
+    component: GeometryConvex,
+  },
+  {
+    path: '/list/geometry-color-lookup',
+    name: 'GeometryColorLookup',
+    meta: {title: "geometry-color-lookup"},
+    component: GeometryColorLookup,
+  },
+  {
+    path: '/list/geometry-colors',
+    name: 'GeometryColors',
+    meta: {title: "geometry-colors"},
+    component: GeometryColors,
+  },
+  {
+    path: '/list/geometries-parametric',
+    name: 'GeometriesParametric',
+    meta: {title: "geometries-parametric"},
+    component: GeometriesParametric,
+  },
+  {
+    path: '/list/webgl-geometry',
+    name: 'WebglGeometry',
+    meta: {title: "webgl-geometry"},
+    component: WebglGeometry,
+  },
+  {
+    path: '/list/framebuffer-texture',
+    name: 'FramebufferTexture',
+    meta: {title: "framebuffer-texture"},
+    component: FramebufferTexture,
+  },
+  {
+    path: '/list/effects-stereo',
+    name: 'EffectsStereo',
+    meta: {title: "effects-stereo"},
+    component: EffectsStereo,
+  },
+  {
+    path: '/list/effects-peppersghost',
+    name: 'EffectsPeppersghost',
+    meta: {title: "effects-peppersghost"},
+    component: EffectsPeppersghost,
+  },
+  {
+    path: '/list/effects-parallaxbarrier',
+    name: 'EffectsParallaxbarrier',
+    meta: {title: "effects-parallaxbarrier"},
+    component: EffectsParallaxbarrier,
+  },
+  {
+    path: '/list/effects-ascii',
+    name: 'EffectsAscii',
+    meta: {title: "effects-ascii"},
+    component: EffectsAscii,
+  },
+  {
+    path: '/list/effects-anaglyph',
+    name: 'EffectsAnaglyph',
+    meta: {title: "effects-anaglyph"},
+    component: EffectsAnaglyph,
+  },
+  {
+    path: '/list/depth-texture',
+    name: 'DepthTexture',
+    meta: {title: "depth-texture"},
+    component: DepthTexture,
+  },
+  {
+    path: '/list/webgl-decals',
+    name: 'WebglDecals',
+    meta: {title: "webgl-decals"},
+    component: WebglDecals,
+  },
+  {
+    path: '/list/clipping-stencil',
+    name: 'ClippingStencil',
+    meta: {title: "clipping-stencil"},
+    component: ClippingStencil,
+  },
+  {
+    path: '/list/clipping-intersection',
+    name: 'ClippingIntersection',
+    meta: {title: "clipping-intersection"},
+    component: ClippingIntersection,
+  },
+  {
+    path: '/list/clipping-advanced',
+    name: 'ClippingAdvanced',
+    meta: {title: "clipping-advanced"},
+    component: ClippingAdvanced,
+  },
+  {
+    path: '/list/webgl-clipping',
+    name: 'WebglClipping',
+    meta: {title: "webgl-clipping"},
+    component: WebglClipping,
+  },
+  {
+    path: '/list/logarithmic-depth-buffer',
+    name: 'LogarithmicDepthBuffer',
+    meta: {title: "logarithmic-depth-buffer"},
+    component: LogarithMicdepthBuffer,
+  },
+  {
+    path: '/list/webgl-camera-cinematic',
+    name: 'WebglCameraCinematic',
+    meta: {title: "webgl-camera-cinematic"},
+    component: WebglCameraCinematic,
+  },
+  {
+    path: '/list/webgl-camera-array',
+    name: 'WebglCameraArray',
+    meta: {title: "webgl-camera-array"},
+    component: WebglCameraArray,
+  },
+  {
+    path: '/list/webgl-camera',
+    name: 'WebglCamera',
+    meta: {title: "webgl-camera"},
+    component: WebglCamera,
+  },
+  {
+    path: '/list/animation-multiple',
+    name: 'AnimationMultiple',
+    meta: {title: "animation-multiple"},
+    component: AnimationMultiple,
+  },
+  {
+    path: '/list/skinning-morph',
+    name: 'SkinningMorph',
+    meta: {title: "skinning-morph"},
+    component: SkinningMorph,
+  },
+  {
+    path: '/list/skinning-ik',
+    name: 'SkinningIk',
+    meta: {title: "skinning-ik"},
+    component: SkinningIk,
+  },
+  {
+    path: '/list/skinning-additive-blending',
+    name: 'SkinningAdditiveBlending',
+    meta: {title: "skinning-additive-blending"},
+    component: SkinningAdditiveBlending,
+  },
+  {
+    path: '/list/skinning-blending',
+    name: 'SkinningBlending',
+    meta: {title: "skinning-blending"},
+    component: SkinningBlending,
+  },
+  {
+    path: '/list/key-frame',
+    name: 'KeyFrame',
+    meta: {title: "animation-keyframes"},
+    component: KeyFrame,
+  },
+  {
+    path: '/list/demo',
+    name: 'Demo',
+    meta: {title: "矩形阵列"},
+    component: Demo,
+  },
+];
+
+export default routerList01;
