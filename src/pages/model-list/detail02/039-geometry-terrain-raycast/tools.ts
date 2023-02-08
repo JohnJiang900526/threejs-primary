8a8cde16870f395b8bf90b00b7e41ed57766b959
蒋昂 <jiangang@powerpms.com>
2023-02-08 09:21:40 +0800

    完成 039-geometry-terrain-raycast 案例的解析

--- src/pages/model-list/detail02/039-geometry-terrain-raycast/tools.ts
+++ src/pages/model-list/detail02/039-geometry-terrain-raycast/tools.ts
@@ -61,11 +61,16 @@ export class Model {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.minDistance = 1000;
     this.controls.maxDistance = 10000;
+    // .maxPolarAngle : Float 够垂直旋转的角度的上限，范围是0到Math.PI，其默认值为Math.PI
     this.controls.maxPolarAngle = (Math.PI / 2);
 
     // 创建helper
     const geometryHelper = new THREE.ConeGeometry(100, 500, 3);
+    // .translate ( x : Float, y : Float, z : Float ) : this
+    // 移动几何体。该操作一般在一次处理中完成，不会循环处理。典型的用法是通过调用 Object3D.rotation 实时旋转几何体
     geometryHelper.translate(0, 50, 0);
+    // .rotateX ( radians : Float ) : this
+    // 在 X 轴上旋转几何体。该操作一般在一次处理中完成，不会循环处理。典型的用法是通过调用 Object3D.rotation 实时旋转几何体
     geometryHelper.rotateX(Math.PI / 2);
     this.helper = new THREE.Mesh(geometryHelper, new THREE.MeshNormalMaterial());
     this.scene.add(this.helper);
@@ -89,6 +94,7 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  // 绑定事件
   private bind() {
     if (this.isMobile()) {
       this.container.onpointermove = null;
@@ -140,8 +146,9 @@ export class Model {
     let quality = 1;
     for (let j = 0; j < 4; j ++) {
       for (let i = 0; i < size; i ++) {
-        const x = i % width, y = ~~(i / width);
-        data[i] += Math.abs(perlin.noise( x / quality, y / quality, z ) * quality * 1.75);
+        const x = i % width;
+        const y = ~~(i / width);
+        data[i] += Math.abs(perlin.noise(x / quality, y / quality, z) * quality * 1.75);
       }
       quality *= 5;
     }
@@ -156,10 +163,8 @@ export class Model {
     let imageData: Uint8ClampedArray;
     let shade: number;
 
-    const vector3 = new THREE.Vector3( 0, 0, 0 );
-
-    const sun = new THREE.Vector3( 1, 1, 1 );
-    sun.normalize();
+    const vector3 = new THREE.Vector3(0, 0, 0);
+    const sun = new THREE.Vector3(1, 1, 1).normalize();
 
     const canvas = document.createElement( 'canvas' );
     canvas.width = width;
@@ -169,26 +174,30 @@ export class Model {
     context.fillStyle = '#000';
     context.fillRect(0, 0, width, height);
 
-    image = context.getImageData( 0, 0, canvas.width, canvas.height );
+    image = context.getImageData(0, 0, canvas.width, canvas.height);
     imageData = image.data;
 
-    for (let i = 0, j = 0; i < imageData.length; i += 4, j ++) {
+    for (let i = 0, j = 0; i < imageData.length; i += 4, j++) {
       vector3.x = data[j - 2] - data[j + 2];
       vector3.y = 2;
       vector3.z = data[j - width * 2] - data[j + width * 2];
+      // 将该向量转换为单位向量（unit vector）， 
+      // 也就是说，将该向量的方向设置为和原向量相同，但是其长度（length）为1
       vector3.normalize();
+      // .dot ( v : Vector3 ) : Float 计算该vector和所传入v的点积
       shade = vector3.dot(sun);
       imageData[i] = (96 + shade * 128) * (0.5 + data[ j ] * 0.007);
       imageData[i + 1] = (32 + shade * 96) * (0.5 + data[ j ] * 0.007);
       imageData[i + 2] = (shade * 96) * (0.5 + data[ j ] * 0.007);
     }
+    // putImageData() 方法将图像数据（从指定的 ImageData 对象）放回画布上
     context.putImageData(image, 0, 0);
 
     const canvasScaled = document.createElement( 'canvas' );
     canvasScaled.width = width * 4;
     canvasScaled.height = height * 4;
 
-    context = canvasScaled.getContext( '2d' ) as CanvasRenderingContext2D;
+    context = canvasScaled.getContext('2d') as CanvasRenderingContext2D;
     context.scale(4, 4);
     context.drawImage(canvas, 0, 0);
 
@@ -201,7 +210,7 @@ export class Model {
       imageData[i + 1] += v;
       imageData[i + 2] += v;
     }
-
+    // putImageData() 方法将图像数据（从指定的 ImageData 对象）放回画布上
     context.putImageData(image, 0, 0);
     return canvasScaled;
   }
@@ -209,8 +218,12 @@ export class Model {
   // 移动事件 触摸事件&手指移动事件
   private onPointerMove(e: Touch | PointerEvent) {
     if (this.renderer && this.camera) {
-      this.pointer.x = (e.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
-      this.pointer.y = - ((e.clientY - 45) / this.renderer.domElement.clientHeight) * 2 + 1;
+      this.pointer.x = (e.clientX / this.width) * 2 - 1;
+      this.pointer.y = (-((e.clientY - 45) / this.height) * 2 + 1);
+      // .setFromCamera ( coords : Vector2, camera : Camera ) : undefined
+      // coords —— 在标准化设备坐标中鼠标的二维坐标 —— X分量与Y分量应当在-1到1之间
+      // camera —— 射线所来源的摄像机
+      // 使用一个新的原点和方向来更新射线
       this.raycaster.setFromCamera(this.pointer, this.camera);
     }
 
