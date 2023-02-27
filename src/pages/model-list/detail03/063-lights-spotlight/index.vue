3316679f85891499e89ad4b4b91fa454ce4761b3
蒋昂 <jiangang@powerpms.com>
2023-02-27 11:34:45 +0800

    完成 063-lights-spotlight 案例解析

--- src/pages/model-list/detail03/063-lights-spotlight/index.vue
+++ src/pages/model-list/detail03/063-lights-spotlight/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import { Icon, Popup } from 'vant';
+  import { Icon, Popup, RadioGroup, Radio, Button, Slider, Checkbox } from 'vant';
+  import { Sketch } from '@ckpack/vue-color';
   import Page from "@/base/page/index.vue";
   import Block from "@/base/block/index.vue";
 </script>
@@ -8,7 +9,7 @@
   <div class="webgl-lights-spotlight-page">
     <Page title="webgl-lights-spotlight">
       <div ref="container" class="key-frame-page-inner">
-        <div v-show="false" class="actions">
+        <div v-show="true" class="actions">
           <Icon @click="openHandle" name="wap-nav" color="#fff"/>
         </div>
       </div>
@@ -17,9 +18,95 @@
     <Popup :show="show" position="right" @click-overlay="closeHandle" :style="{ height: '100%', width: '80%' }">
       <Page @click-left="closeHandle" :default-click="false" title="模型设置">
         <div class="settings-content">
-          <Block title="控制"></Block>
+          <Block title="设置Shadows">
+            <div class="item-param">
+              <Checkbox v-model="shadows" @change="shadowsChange">Shadows</Checkbox>
+            </div>
+          </Block>
+
+          <Block title="设置颜色">
+            <div @click="openColorHandle" class="color-block" :style="{'background-color': color}"></div>
+          </Block>
+
+          <Block title="设置Map">
+            <radio-group v-model="map" @change="mapChange">
+              <div v-for="item in maps" :key="item" class="item-param">
+                <radio :name="item">{{item}}</radio>
+              </div>
+            </radio-group>
+          </Block>
+
+          <Block title="设置intensity">
+            <div class="item-param slider">
+              <Slider 
+                v-model="intensity" :min="0" :max="10" :step="0.01" 
+                @change="intensityChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <Block title="设置distance">
+            <div class="item-param slider">
+              <Slider 
+                v-model="distance" :min="50" :max="200" :step="0.01" 
+                @change="distanceChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <Block title="设置angle">
+            <div class="item-param slider">
+              <Slider 
+                v-model="angle" :min="0" :max="Math.PI/3" :step="0.01" 
+                @change="angleChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <Block title="设置penumbra">
+            <div class="item-param slider">
+              <Slider 
+                v-model="penumbra" :min="0" :max="1" :step="0.01" 
+                @change="penumbraChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <Block title="设置decay">
+            <div class="item-param slider">
+              <Slider 
+                v-model="decay" :min="1" :max="2" :step="0.01" 
+                @change="decayChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <Block title="设置focus">
+            <div class="item-param slider">
+              <Slider 
+                v-model="focus" :min="0" :max="1" :step="0.01" 
+                @change="focusChange" 
+                bar-height="4px" active-color="#238bfe"/>
+            </div>
+          </Block>
+
+          <div style="height: 200px"></div>
+
         </div>
       </Page>
+    </Popup>
+
+    <Popup :show="showColor" position="bottom" 
+      @click-overlay="closeColorHandle" closeable
+      :style="{ height: 'auto', width: '100%'}">
+      <div class = "color-content">
+        <div class="color-content-body">  
+          <Sketch v-model="color" width="100%"/>
+        </div>
+        <div class="color-content-footer">
+          <Button @click="closeColorHandle" size="small" type="primary" block>确定</Button>
+        </div>
+      </div>
     </Popup>
   </div>
 </template>
@@ -33,6 +120,17 @@ export default defineComponent({
   data() {
     return {
       show: false,
+      showColor: false,
+      map: "disturb.jpg",
+      color: "#ffffff",
+      intensity: 5,
+      distance: 100,
+      angle: 0.52,
+      penumbra: 1,
+      decay: 2,
+      focus: 1,
+      shadows: true,
+      maps: ['disturb.jpg', 'colors.png', 'uv_grid_opengl.jpg']
     };
   },
   mounted() {
@@ -44,6 +142,59 @@ export default defineComponent({
       objModel = new Model(this.$refs.container as HTMLDivElement);
       objModel.init();
     },
+    shadowsChange(shadows: boolean) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setShadows(shadows);
+      }
+    },
+    focusChange(focus: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setFocus(focus);
+      }
+    },
+    decayChange(decay: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setDecay(decay);
+      }
+    },
+    penumbraChange(penumbra: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setPenumbra(penumbra);
+      }
+    },
+    angleChange(angle: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setAngle(angle);
+      }
+    },
+    mapChange(map: string) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setMap(map);
+      }
+    },
+    colorChange(color: string) {
+      if (objModel) {
+        objModel.setColor(color);
+      }
+    },
+    intensityChange(intensity: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setIntensity(intensity);
+      }
+    },
+    distanceChange(distance: number) {
+      if (objModel) {
+        this.closeHandle();
+        objModel.setDistance(distance);
+      }
+    },
     // 打开设置面板
     openHandle(e: MouseEvent) {
       e.preventDefault();
@@ -54,11 +205,27 @@ export default defineComponent({
     // 关闭
     closeHandle() {
       this.show = false;
+    },
+    openColorHandle() {
+      this.showColor = true;
+    },
+    closeColorHandle() {
+      this.showColor = false;
     }
   },
   // 卸载前函数
   beforeUnmount() {
     objModel = null;
+  },
+  watch: {
+    color() {
+      const toString = Object.prototype.toString;
+      if (toString.call(this.color) === "[object Object]") {
+        // @ts-ignore
+        this.color = this.color.hex;
+      }
+      this.colorChange(this.color);
+    }
   }
 });
 </script>
@@ -110,6 +277,31 @@ export default defineComponent({
         &.slider {
           padding: 15px 20px;
         }
+      }
+
+      .color-block {
+        width: 120px;
+        height: 35px;
+        border-radius: 3px;
+        border: 1px solid #ddd;
+      }
+    }
+
+    .color-content {
+      display: flex;
+      flex-direction: column;
+      .color-content-body {
+        flex: 1;
+        padding: 20px 10px;
+        box-sizing: border-box;
+      }
+
+      .color-content-footer {
+        .top-line();
+        height: 55px;
+        text-align: center;
+        padding: 10px 10px;
+        box-sizing: border-box;
       }
     }
   }
--- src/pages/model-list/detail03/063-lights-spotlight/tools.ts
+++ src/pages/model-list/detail03/063-lights-spotlight/tools.ts
@@ -14,6 +14,7 @@ export class Model {
   private stats: null | Stats;
   private spotLight: THREE.SpotLight
   private lightHelper: null | THREE.SpotLightHelper
+  private textures: {[key: string]: THREE.Texture}
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -25,6 +26,7 @@ export class Model {
     this.stats = null;
     this.spotLight = new THREE.SpotLight();
     this.lightHelper = null;
+    this.textures = {};
   }
 
   // 初始化方法入口
@@ -34,7 +36,7 @@ export class Model {
     this.scene.background = new THREE.Color(0x000000);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 1, 1000);
+    this.camera = new THREE.PerspectiveCamera(40, this.aspect, 1, 1000);
     this.camera.position.set(70, 50, 10);
 
     // 加载模型
@@ -55,7 +57,6 @@ export class Model {
     controls.update();
 
     this.initStats();
-    this.animate();
     this.resize();
   }
 
@@ -65,25 +66,138 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  // 设置纹理
+  setMap(map: string) {
+    // @ts-ignore
+    this.spotLight.map = this.textures[map];
+  }
+
+  // 设置颜色
+  setColor (color: string) {
+    this.spotLight.color.setStyle(color);
+  }
+
+  // 设置强度
+  setIntensity(intensity: number) {
+    this.spotLight.intensity = intensity;
+  }
+
+  // 设置距离
+  setDistance(distance: number) {
+    this.spotLight.distance = distance;
+  }
+
+  // 设置角度
+  setAngle(angle: number) {
+    this.spotLight.angle = angle;
+  }
+
+  // 设置半阴影
+  setPenumbra(penumbra: number) {
+    this.spotLight.penumbra = penumbra;
+  }
+
+  // 设置衰变
+  setDecay(decay: number) {
+    this.spotLight.decay = decay;
+  }
+
+  setFocus(focus: number) {
+    this.spotLight.shadow.focus = focus;
+  }
+
+  setShadows(enabled: boolean) {
+    if (this.renderer) {
+      this.renderer.shadowMap.enabled = enabled;
+
+      this.scene.traverse((child) => {
+        const obj = child as THREE.Mesh;
+        if (obj.material) {
+          (obj.material as THREE.Material).needsUpdate = true;
+        }
+      });
+    }
+  }
+
   // 加载模型
   private loadModel() {
+    const loader = new PLYLoader();
+    const url = "/examples/models/ply/binary/Lucy100k.ply";
 
+    loader.load(url, (geometry) => {
+      geometry.scale(0.024, 0.024, 0.024);
+      geometry.computeVertexNormals();
+
+      const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial());
+      mesh.rotation.y = -Math.PI/2;
+      mesh.position.y = 18;
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+      this.scene.add(mesh);
+    });
   }
 
   // 创建光源
   private createLight() {
-    
+    const loader = new THREE.TextureLoader().setPath("/examples/textures/");
+    const filenames = ['disturb.jpg', 'colors.png', 'uv_grid_opengl.jpg'];
+
+    filenames.forEach((filename) => {
+      const texture = loader.load(filename);
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+      texture.encoding = THREE.sRGBEncoding;
+      this.textures[filename] = texture;
+    });
+
+    this.spotLight = new THREE.SpotLight(0xffffff, 5);
+    this.spotLight.position.set(25, 50, 25);
+    this.spotLight.angle = Math.PI/6;
+    this.spotLight.penumbra = 1;
+    this.spotLight.decay = 2;
+    this.spotLight.distance = 100;
+    // @ts-ignore
+    this.spotLight.map = this.textures['disturb.jpg'];
+
+    this.spotLight.castShadow = true;
+    this.spotLight.shadow.mapSize.width = 1024;
+    this.spotLight.shadow.mapSize.height = 1024;
+    this.spotLight.shadow.camera.near = 10;
+    this.spotLight.shadow.camera.far = 200;
+    this.spotLight.shadow.focus = 1;
+    this.scene.add(this.spotLight);
+
+    // 帮助
+    this.lightHelper = new THREE.SpotLightHelper(this.spotLight);
+		this.scene.add(this.lightHelper);
+
+    // geometry
+    const geometry = new THREE.PlaneGeometry(1000, 1000);
+    const material = new THREE.MeshLambertMaterial({ color: 0x808080 });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(0, -1, 0);
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+    this.scene.add(mesh);
   }
 
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
+    // - enabled: 如果设置开启，允许在场景中使用阴影贴图。默认是 false
     this.renderer.shadowMap.enabled = true;
+    // - type: 定义阴影贴图类型 (未过滤, 关闭部分过滤, 关闭部分双线性过滤), 可选值有
+    // THREE.BasicShadowMap
+    // THREE.PCFShadowMap (默认)
+    // THREE.PCFSoftShadowMap
+    // THREE.VSMShadowMap
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
+    // 定义渲染器的输出编码。默认为THREE.LinearEncoding
     this.renderer.outputEncoding = THREE.sRGBEncoding;
-
+    // 默认是NoToneMapping 色调映射
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    // 色调映射的曝光级别。默认是1
     this.renderer.toneMappingExposure = 1;
 
     this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -101,7 +215,19 @@ export class Model {
   }
 
   private render() {
+    const time = performance.now()/3000;
 
+    this.spotLight.position.x = Math.cos(time) * 25;
+    this.spotLight.position.z = Math.sin(time) * 25;
+    if (this.lightHelper) { this.lightHelper.update(); }
+
+    // 统计信息更新
+    if (this.stats) { this.stats.update(); }
+
+    // 执行渲染
+    if (this.camera && this.renderer) {
+      this.renderer.render(this.scene, this.camera);
+    }
   }
 
   // 持续动画
