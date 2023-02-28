c6d3bbbead8a8cb5b70741300efe3ad95cfdbc4b
蒋昂 <jiangang@powerpms.com>
2023-02-28 09:50:16 +0800

    完成 064-lights-spotlights 案例的解析

--- src/pages/model-list/detail03/064-lights-spotlights/tools.ts
+++ src/pages/model-list/detail03/064-lights-spotlights/tools.ts
@@ -12,12 +12,9 @@ export class Model {
   private renderer: null | THREE.WebGLRenderer;
   private camera: null | THREE.PerspectiveCamera;
   private stats: null | Stats;
-  private matFloor: THREE.MeshPhongMaterial
-  private matBox: THREE.MeshPhongMaterial
-  private geoFloor: THREE.PlaneGeometry
-  private geoBox: THREE.BoxGeometry
-  private mshFloor: THREE.Mesh
-  private mshBox: THREE.Mesh
+
+  private floor: THREE.Mesh
+  private box: THREE.Mesh
   private spotLight1: THREE.SpotLight
   private spotLight2: THREE.SpotLight
   private spotLight3: THREE.SpotLight
@@ -35,16 +32,11 @@ export class Model {
     this.camera = null;
     this.stats = null;
 
-    this.matFloor = new THREE.MeshPhongMaterial();
-    this.matBox = new THREE.MeshPhongMaterial();
-    this.geoFloor = new THREE.PlaneGeometry();
-    this.geoBox = new THREE.BoxGeometry();
-    this.mshFloor = new THREE.Mesh();
-    this.mshBox = new THREE.Mesh();
-
-    this.spotLight1 = new THREE.SpotLight(0xFF7F00);
-    this.spotLight2 = new THREE.SpotLight(0x00FF7F);
-    this.spotLight3 = new THREE.SpotLight(0x7F00FF);
+    this.floor = new THREE.Mesh();
+    this.box = new THREE.Mesh();
+    this.spotLight1 = new THREE.SpotLight();
+    this.spotLight2 = new THREE.SpotLight();
+    this.spotLight3 = new THREE.SpotLight();
     this.lightHelper1 = new THREE.SpotLightHelper(this.spotLight1);
     this.lightHelper2 = new THREE.SpotLightHelper(this.spotLight2);
     this.lightHelper3 = new THREE.SpotLightHelper(this.spotLight3);
@@ -91,51 +83,67 @@ export class Model {
   // 创建floor and box
   private createFloorAndBox() {
     // 箱子
-    this.matBox = new THREE.MeshPhongMaterial({color: 0xaaaaaa});
-    this.geoBox = new THREE.BoxGeometry(3, 1, 2);
-    this.mshBox = new THREE.Mesh(this.geoBox, this.matBox);
-    this.mshBox.castShadow = true;
-    this.mshBox.receiveShadow = true;
-    this.mshBox.position.set(0, 5, 0);
-    this.scene.add(this.mshBox);
+    (() => {
+      const material = new THREE.MeshPhongMaterial({color: 0xaaaaaa});
+      const geometry = new THREE.BoxGeometry(3, 1, 2);
+      this.box = new THREE.Mesh(geometry, material);
+      this.box.castShadow = true;
+      this.box.receiveShadow = true;
+      this.box.position.set(0, 5, 0);
+      this.scene.add(this.box);
+    })();
 
     // 地板
-    this.matFloor = new THREE.MeshPhongMaterial({color: 0x808080});
-    this.geoFloor = new THREE.PlaneGeometry(2000, 2000);
-    
-    this.mshFloor = new THREE.Mesh(this.geoFloor, this.matFloor);
-    this.mshFloor.rotation.x = -Math.PI * 0.5;
-    this.mshFloor.receiveShadow = true;
-    this.mshFloor.position.set(0, -0.05, 0);
-    this.scene.add(this.mshFloor);
+    (() => {
+      const material = new THREE.MeshPhongMaterial({color: 0x808080});
+      const geometry = new THREE.PlaneGeometry(2000, 2000);
+      
+      this.floor = new THREE.Mesh(geometry, material);
+      this.floor.rotation.x = -Math.PI * 0.5;
+      this.floor.receiveShadow = true;
+      this.floor.position.set(0, -0.05, 0);
+      this.scene.add(this.floor);
+    })();
   }
 
   // 创建灯光
   private createLight() {
+    // 第1个聚光灯
     this.spotLight1 = this.createSpotlight(0xFF7F00);
     this.spotLight1.position.set(15, 40, 45);
 
+    // 第2个聚光灯
     this.spotLight2 = this.createSpotlight(0x00FF7F);
     this.spotLight2.position.set(0, 40, 35);
 
+    // 第3个聚光灯
     this.spotLight3 = this.createSpotlight(0x7F00FF);
     this.spotLight3.position.set(-15, 40, 45);
+
+    // 漫散射光
+    const light = new THREE.AmbientLight(0x111111);
 
     this.lightHelper1 = new THREE.SpotLightHelper(this.spotLight1);
     this.lightHelper2 = new THREE.SpotLightHelper(this.spotLight2);
     this.lightHelper3 = new THREE.SpotLightHelper(this.spotLight3);
 
-    this.scene.add(this.spotLight1, this.spotLight2, this.spotLight3);
+    this.scene.add(this.spotLight1, this.spotLight2, this.spotLight3, light);
     this.scene.add(this.lightHelper1, this.lightHelper2, this.lightHelper3);
-    this.scene.add(new THREE.AmbientLight(0x111111));
   }
 
-  private createSpotlight(color: number) {
+  // 创建聚光灯
+  private createSpotlight(color: number = 0xffffff) {
     const light = new THREE.SpotLight(color, 2);
+
+    // castShadow -此属性设置为 true 聚光灯将投射阴影
     light.castShadow = true;
+    // angle - 光线散射角度，最大为Math.PI/2
     light.angle = 0.3;
+    // penumbra - 聚光锥的半影衰减百分比。在0和1之间的值。默认为0
     light.penumbra = 0.2;
+    // decay - 沿着光照距离的衰减量
     light.decay = 2;
+    // distance - 从光源发出光的最大距离，其强度根据光源的距离线性衰减
     light.distance = 50;
 
     return light;
@@ -144,11 +152,13 @@ export class Model {
   // 执行动画
   private tween(light: THREE.SpotLight) {
     new TWEEN.Tween(light).to({
+      // 光线散射角度
       angle: (Math.random() * 0.7) + 0.1,
-      penumbra: Math.random() + 1
+      // 聚光锥的半影衰减百分比
+      penumbra: Math.random() + 1,
     }, Math.random() * 3000 + 2000).easing(TWEEN.Easing.Quadratic.Out).start();
 
-    new TWEEN.Tween(light.position ).to({
+    new TWEEN.Tween(light.position).to({
       x: (Math.random() * 30) - 15,
       y: (Math.random() * 10) + 15,
       z: (Math.random() * 30) - 15,
@@ -158,8 +168,15 @@ export class Model {
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
+    // - enabled: 如果设置开启，允许在场景中使用阴影贴图。默认是 false
     this.renderer.shadowMap.enabled = true;
+    // - type: 定义阴影贴图类型 (未过滤, 关闭部分过滤, 关闭部分双线性过滤), 可选值有
+    // THREE.BasicShadowMap 能够给出没有经过过滤的阴影映射 —— 速度最快，但质量最差
+    // THREE.PCFShadowMap (默认) 使用Percentage-Closer Filtering (PCF)算法来过滤阴影映射
+    // THREE.PCFSoftShadowMap 和PCFShadowMap一样使用 Percentage-Closer Filtering (PCF) 算法过滤阴影映射，但在使用低分辨率阴影图时具有更好的软阴影
+    // THREE.VSMShadowMap 使用Variance Shadow Map (VSM)算法来过滤阴影映射。当使用VSMShadowMap时，所有阴影接收者也将会投射阴影
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    // 定义渲染器的输出编码。默认为THREE.LinearEncoding
     this.renderer.outputEncoding = THREE.sRGBEncoding;
 
     this.renderer.setPixelRatio(window.devicePixelRatio);
