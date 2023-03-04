985193ed3cfdd1a7535d845f5225088a60e29b83
蒋昂 <jiangang@powerpms.com>
2023-03-04 21:54:11 +0800

    完成 075-loader-3mf-materials 案例开发

--- src/pages/model-list/detail03/075-loader-3mf-materials/tools.ts
+++ src/pages/model-list/detail03/075-loader-3mf-materials/tools.ts
@@ -30,8 +30,38 @@ export class Model {
 
   // 初始化方法入口
   init() {
-   
+    // 场景
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xa0a0a0);
+    this.scene.fog = new THREE.Fog(0xa0a0a0, 10, 500);
 
+    // 相机
+    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 1, 5000);
+    this.camera.position.set(-50, 50, 50);
+    this.scene.add(this.camera);
+
+    // 光照
+    this.createLight();
+
+    // 加载模型
+    this.loadModel();
+
+    // 创建地板
+    this.createFloor();
+
+    // 渲染器
+    this.createRenderer();
+
+    // 创建控制器
+    this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
+    this.controls.minDistance = 50;
+    this.controls.maxDistance = 200;
+    this.controls.enablePan = false;
+    this.controls.target.set(0, 20, 0);
+    this.controls.addEventListener('change', () => { this.render(); });
+    this.controls.update();
+
+    this.render();
     this.resize();
   }
 
@@ -41,9 +71,66 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  private createLight() {
+    // 半球光
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 100, 0);
+    this.scene.add(hemiLight);
+
+    // 直线光
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(-0, 40, 50);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = -25;
+    dirLight.shadow.camera.left = -25;
+    dirLight.shadow.camera.right = 25;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.set(1024, 1024);
+    this.scene.add(dirLight);
+  }
+
+  private createFloor() {
+    const ground = new THREE.Mesh(
+      new THREE.PlaneGeometry( 1000, 1000 ), 
+      new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+    );
+    ground.rotation.x = -Math.PI / 2;
+    ground.position.y = 11;
+    ground.receiveShadow = true;
+    this.scene.add(ground);
+  }
+
+  private loadModel() {
+    const manager = new THREE.LoadingManager(() => {
+      this.render();
+    });
+
+    const loader = new ThreeMFLoader(manager);
+    const url = '/examples/models/3mf/truck.3mf';
+    const toast = showLoadingToast({
+      message: '模型加载中...',
+      forbidClick: true,
+      loadingType: 'spinner',
+    });
+    loader.load(url, (obj) => {
+      toast.close();
+      // z-up conversion
+      obj.quaternion.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0));
+      obj.traverse((child) => { child.castShadow = false; });
+      this.scene.add(obj);
+    }, undefined, () => {
+      toast.close();
+    });
+  }
+
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
@@ -92,6 +179,7 @@ export class Model {
       if (this.renderer) {
         this.renderer.setSize(this.width, this.height);
       }
+      this.render();
     };
   }
 }
