d90d0b49e17f1582dab007f172a70cc0b1cf1a59
蒋昂 <jiangang@powerpms.com>
2023-03-05 09:07:37 +0800

    完成 075-loader-3mf-materials 案例解析

--- src/pages/model-list/detail03/075-loader-3mf-materials/tools.ts
+++ src/pages/model-list/detail03/075-loader-3mf-materials/tools.ts
@@ -33,11 +33,19 @@ export class Model {
     // 场景
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xa0a0a0);
+    // 雾（Fog）这个类中的参数定义了线性雾。也就是说，雾的密度是随着距离线性增大的
+    // Fog( color : Integer, near : Float, far : Float )
+    // 颜色参数传入Color构造函数中，来设置颜色属性。颜色可以是一个十六进制的整型数，或者是CSS风格的字符串。
+    // .isFog : Boolean 只读属性 是否是雾类型
+    // .name : String 对象的名称，可选、不必唯一。默认值是一个空字符串
+    // .color : Color 雾的颜色。比如说，如果将其设置为黑色，远处的物体将被渲染成黑色
+    // .near : Float 开始应用雾的最小距离。距离小于活动摄像机“near”个单位的物体将不会被雾所影响 默认值是1
+    // .far : Float 结束计算、应用雾的最大距离，距离大于活动摄像机“far”个单位的物体将不会被雾所影响。默认值是1000
     this.scene.fog = new THREE.Fog(0xa0a0a0, 10, 500);
 
     // 相机
     this.camera = new THREE.PerspectiveCamera(60, this.aspect, 1, 5000);
-    this.camera.position.set(-50, 50, 50);
+    this.camera.position.set(-60, 60, 60);
     this.scene.add(this.camera);
 
     // 光照
@@ -72,16 +80,17 @@ export class Model {
   }
 
   private createLight() {
-    // 半球光
+    // 半球光 半球光（HemisphereLight）半球光不能投射阴影
+    // 光源直接放置于场景之上，光照颜色从天空光线颜色渐变到地面光线颜色 
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
     hemiLight.position.set(0, 100, 0);
     this.scene.add(hemiLight);
 
     // 直线光
     const dirLight = new THREE.DirectionalLight(0xffffff);
-    dirLight.position.set(-0, 40, 50);
+    dirLight.position.set(-0, 50, 50);
     dirLight.castShadow = true;
-    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.top = 25;
     dirLight.shadow.camera.bottom = -25;
     dirLight.shadow.camera.left = -25;
     dirLight.shadow.camera.right = 25;
@@ -92,10 +101,13 @@ export class Model {
   }
 
   private createFloor() {
-    const ground = new THREE.Mesh(
-      new THREE.PlaneGeometry( 1000, 1000 ), 
-      new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-    );
+    const geometry = new THREE.PlaneGeometry(1000, 1000);
+    const material = new THREE.MeshPhongMaterial({ 
+      color: 0x999999,
+      // 渲染此材质是否对深度缓冲区有任何影响。默认为true
+      depthWrite: false
+    });
+    const ground = new THREE.Mesh(geometry, material);
     ground.rotation.x = -Math.PI / 2;
     ground.position.y = 11;
     ground.receiveShadow = true;
@@ -116,8 +128,10 @@ export class Model {
     });
     loader.load(url, (obj) => {
       toast.close();
-      // z-up conversion
+      // z-up conversion .setFromEuler ( euler : Euler ) : this
+      // 从由 Euler 角所给定的旋转来设置该四元数
       obj.quaternion.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0));
+      // 设置不产生阴影
       obj.traverse((child) => { child.castShadow = false; });
       this.scene.add(obj);
     }, undefined, () => {
@@ -130,6 +144,8 @@ export class Model {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.outputEncoding = THREE.sRGBEncoding;
     this.renderer.shadowMap.enabled = true;
+    // PCFSoftShadowMap 和PCFShadowMap一样使用 Percentage-Closer Filtering (PCF) 算法过滤阴影映射，
+    // 但在使用低分辨率阴影图时具有更好的软阴影
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
