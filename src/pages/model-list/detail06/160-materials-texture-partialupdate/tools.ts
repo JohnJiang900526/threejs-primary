cbe4d07278dd4cee6a3d48768b30056c4dff80af
蒋昂 <jiangang@powerpms.com>
2023-04-25 11:21:15 +0800

    完成 160-materials-texture-partialupdate 案例开发

--- src/pages/model-list/detail06/160-materials-texture-partialupdate/tools.ts
+++ src/pages/model-list/detail06/160-materials-texture-partialupdate/tools.ts
@@ -12,9 +12,12 @@ export class Model {
   private camera: null | THREE.PerspectiveCamera;
   private stats: null | Stats;
 
-  private mesh: THREE.Mesh
-  private material: THREE.MeshBasicMaterial
-  private drawStart: THREE.Vector2
+  private clock: THREE.Clock
+  private dataTexture: THREE.DataTexture
+  private diffuseMap: THREE.Texture
+  private last: number
+  private position: THREE.Vector2
+  private color: THREE.Color
   constructor(container: HTMLDivElement, canvas: HTMLCanvasElement) {
     this.container = container;
     this.canvas = canvas;
@@ -26,9 +29,12 @@ export class Model {
     this.camera = null;
     this.stats = null;
 
-    this.mesh = new THREE.Mesh();
-    this.material = new THREE.MeshBasicMaterial();
-    this.drawStart = new THREE.Vector2();
+    this.clock = new THREE.Clock();
+    this.dataTexture = new THREE.DataTexture();
+    this.diffuseMap = new THREE.Texture();
+    this.last = 0;
+    this.position = new THREE.Vector2();
+    this.color = new THREE.Color();
   }
 
   init() {
@@ -37,20 +43,17 @@ export class Model {
     this.scene.background = new THREE.Color(0x000000);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 2000);
-    this.camera.position.z = 500;
+    this.camera = new THREE.PerspectiveCamera(70, this.aspect, 0.01, 10);
+    this.camera.position.z = 3.5;
 
-    // 模型
-    const geometry = new THREE.BoxGeometry(100, 100, 100);
-    this.mesh = new THREE.Mesh(geometry, this.material);
-    this.scene.add(this.mesh);
+    this.generateTexture();
+    this.generateMap();
+    this.generateMesh();
 
-    this.setupDrawing();
     // 渲染器
     this.createRenderer();
 
     this.initStats();
-    this.animate();
     this.resize();
   }
 
@@ -60,44 +63,36 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
-  private draw(context: CanvasRenderingContext2D, x: number, y: number) {
-    context.moveTo(this.drawStart.x, this.drawStart.y);
-    context.strokeStyle = '#000000';
-    context.lineTo(x, y);
-    context.stroke();
+  private generateTexture() {
+    const width = 32;
+    const height = 32;
+    const data = new Uint8Array(width * height * 4);
 
-    this.drawStart.set(x, y);
-    if (this.material.map) {
-      this.material.map.needsUpdate = true;
-    }
+    this.dataTexture = new THREE.DataTexture(data, width, height);
+    return this.dataTexture;
   }
 
-  private setupDrawing() {
-    let paint = false;
+  private generateMesh() {
+    const geometry = new THREE.PlaneGeometry(2, 2);
+    const material = new THREE.MeshBasicMaterial({ map: this.diffuseMap });
 
-    const context = this.canvas.getContext("2d") as CanvasRenderingContext2D ;
-    context.fillStyle = '#FFFFFF';
-    context.fillRect(0, 0, 128, 128);
-    this.material.map = new THREE.CanvasTexture(this.canvas);
+    const mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(mesh);
+  }
 
-    this.canvas.onpointerdown = (e) => {
-      paint = true;
-      this.drawStart.set(e.offsetX, e.offsetY);
-    };
+  private generateMap() {
+    const loader = new THREE.TextureLoader();
+    const url = "/examples/textures/floors/FloorsCheckerboard_S_Diffuse.jpg";
 
-    this.canvas.onpointermove = (e) => {
-      if (paint) {
-        this.draw(context, e.offsetX, e.offsetY);
-      }
-    };
-
-    this.canvas.onpointerup = () => {
-      paint = false;
-    };
-
-    this.canvas.onpointerleave = () => {
-      paint = false;
-    };
+    this.diffuseMap = loader.load(url, () => {
+      this.animate();
+    });
+    // 当一个纹素覆盖小于一个像素时，贴图将如何采样。
+    // 默认值为THREE.LinearMipmapLinearFilter， 它将使用mipmapping以及三次线性滤镜
+    this.diffuseMap.minFilter = THREE.LinearFilter;
+    // 是否为纹理生成mipmap（如果可用）。默认为true。 如果你手动生成mipmap，请将其设为false
+    this.diffuseMap.generateMipmaps = false;
+    return this.diffuseMap;
   }
 
   // 创建渲染器
@@ -118,17 +113,57 @@ export class Model {
     this.container.appendChild(this.stats.domElement);
   }
 
+  // 核心
+  private updateDataTexture(texture: THREE.DataTexture) {
+    this.color.setHex(Math.random() * 0xffffff);
+
+    const size = texture.image.width * texture.image.height;
+		const data = texture.image.data;
+
+    const r = Math.floor(this.color.r * 255);
+    const g = Math.floor(this.color.g * 255);
+    const b = Math.floor(this.color.b * 255);
+
+    for (let i = 0; i < size; i++) {
+      const stride = i * 4;
+      data[stride] = r;
+      data[stride + 1] = g;
+      data[stride + 2] = b;
+      data[stride + 3] = 1;
+    }
+  }
+
   // 持续动画
   private animate() {
     window.requestAnimationFrame(() => { this.animate(); });
 
-    this.mesh.rotation.x += 0.005;
-    this.mesh.rotation.y += 0.005;
+    // .getElapsedTime () : Float
+    // 获取自时钟启动后的秒数，同时将 .oldTime 设置为当前时间
+    // 如果 .autoStart 设置为 true 且时钟并未运行，则该方法同时启动时钟
+    const timer = this.clock.getElapsedTime();
+    if (timer - this.last > 0.1) {
+      this.last = timer;
+
+      this.position.x = (32 * THREE.MathUtils.randInt(1, 16)) - 32;
+      this.position.y = (32 * THREE.MathUtils.randInt(1, 16)) - 32;
+
+      this.updateDataTexture(this.dataTexture);
+
+      if (this.renderer && this.camera) {
+        // .copyTextureToTexture ( position : Vector2, srcTexture : Texture, dstTexture : Texture, level : Number ) : undefined
+        // 将纹理的所有像素复制到一个已有的从给定位置开始的纹理中
+        this.renderer.copyTextureToTexture(
+          this.position, 
+          this.dataTexture, 
+          this.diffuseMap
+        );
+      }
+    }
     
     this.stats?.update();
     
     // 执行渲染
-    if (this.renderer && this.scene && this.camera) {
+    if (this.renderer && this.camera) {
       this.renderer.render(this.scene, this.camera);
     }
   }
