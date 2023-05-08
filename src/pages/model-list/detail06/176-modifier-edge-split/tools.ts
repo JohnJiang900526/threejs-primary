c3bc9689f1e3a88d6ab759dd3f183727e113b93b
蒋昂 <jiangang@powerpms.com>
2023-05-08 10:54:03 +0800

    完成 176-modifier-edge-split 案例开发

--- src/pages/model-list/detail06/176-modifier-edge-split/tools.ts
+++ src/pages/model-list/detail06/176-modifier-edge-split/tools.ts
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
+import { showLoadingToast } from 'vant';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { EdgeSplitModifier } from 'three/examples/jsm/modifiers/EdgeSplitModifier';
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils';
 
 export class Model {
   private width: number;
@@ -13,14 +18,18 @@ export class Model {
   private stats: null | Stats;
 
   private controls: null | OrbitControls;
+  private gui: GUI;
+  private modifier: EdgeSplitModifier
   private mesh: THREE.Mesh
-  private target: THREE.Mesh
-  private spherical: THREE.Spherical
-  private rotationMatrix: THREE.Matrix4
-  private targetQuaternion: THREE.Quaternion
-  private clock: THREE.Clock
-  private speed: number
-  private timer: any
+  private baseGeometry: THREE.BufferGeometry
+  private map: THREE.Texture
+  private params: {
+    smoothShading: boolean,
+    edgeSplit: boolean,
+    cutOffAngle: number,
+    showMap: boolean,
+    tryKeepNormals: boolean,
+  }
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -32,33 +41,46 @@ export class Model {
     this.stats = null;
 
     this.controls = null;
+    this.gui = new GUI({
+      container: this.container,
+      autoPlace: true,
+      title: "控制面板",
+    });
+    this.modifier = new EdgeSplitModifier();
     this.mesh = new THREE.Mesh();
-    this.target = new THREE.Mesh();
-    this.spherical = new THREE.Spherical();
-    this.rotationMatrix = new THREE.Matrix4();
-    this.targetQuaternion = new THREE.Quaternion();
-    this.clock = new THREE.Clock();
-    this.speed = 2;
-    this.timer = -1;
+    this.baseGeometry = new THREE.BufferGeometry();
+    this.map = new THREE.Texture();
+    this.params = {
+      showMap: false,
+      smoothShading: true,
+      edgeSplit: true,
+      cutOffAngle: 20,
+      tryKeepNormals: true,
+    };
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
+    this.scene.add(new THREE.HemisphereLight(0xffffff, 0x444444));
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(70, this.aspect, 0.01, 10);
-    this.camera.position.set(0, 0, 6.5);
+    this.camera = new THREE.PerspectiveCamera(75, this.aspect);
+    this.camera.position.set(0, 0, 10);
 
-    this.generateMesh();
+    this.loadModel();
+    this.loadTexture();
     // 渲染器
     this.createRenderer();
 
     // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25;
+    this.controls.rotateSpeed = 0.35;
+    this.controls.minZoom = 1;
 
-    this.generateTarget();
+    this.setUpGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -70,65 +92,103 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
-  private generateMesh() {
-    {
-      // 圆锥缓冲几何体（ConeGeometry）
-      // 一个用于生成圆锥几何体的类
-      const geometry = (new THREE.ConeGeometry(0.1, 0.5, 8)).rotateX(Math.PI * 0.5);
-      const material = new THREE.MeshNormalMaterial();
-
-      this.mesh = new THREE.Mesh(geometry, material);
-      this.scene.add(this.mesh);
-    }
-
-    {
-      // 小的圆球
-      const geometry = new THREE.SphereGeometry(0.05);
-      const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
-      this.target = new THREE.Mesh(geometry, material);
-      this.scene.add(this.target);
-    }
-
-    {
-      // 大的请求网格
-      const geometry = new THREE.SphereGeometry(2, 32, 32);
-      const material = new THREE.MeshBasicMaterial({ 
-        opacity: 0.3,
-        color: 0xcccccc, 
-        wireframe: true, 
-        transparent: true, 
-      });
-      const sphere = new THREE.Mesh( geometry, material );
-      this.scene.add(sphere);
-    }
+  private setUpGUI() {
+    this.gui.add(this.params, 'showMap').onFinishChange(() => {
+      this.updateMesh();
+    });
+    this.gui.add(this.params, 'smoothShading').onFinishChange(() => {
+      this.updateMesh();
+    });
+    this.gui.add(this.params, 'edgeSplit').onFinishChange(() => {
+      this.updateMesh();
+    });
+    this.gui.add(this.params, 'cutOffAngle', 0, 180).onFinishChange(() => {
+      this.updateMesh();
+    });
+    this.gui.add(this.params, 'tryKeepNormals').onFinishChange(() => {
+      this.updateMesh();
+    });
   }
 
-  // 核心
-  private generateTarget() {
-    // 球坐标（Spherical）
-    // Spherical( radius : Float, phi : Float, theta : Float )
-    // radius - 半径值，或者说从该点到原点的 Euclidean distance（欧几里得距离，即直线距离）。默认值为1.0
-    // phi - 与 y (up) 轴的极角（以弧度为单位）。 默认值为 0
-    // theta - 绕 y (up) 轴的赤道角(方位角)（以弧度为单位）。 默认值为 0
-    // 极角（phi）位于正 y 轴和负 y 轴上。赤道角(方位角)（theta）从正 z 开始
-    this.spherical.theta = Math.random() * Math.PI * 2;
-    this.spherical.phi = Math.acos((2 * Math.random()) - 1);
-    this.spherical.radius = 2;
-    // .setFromSpherical ( s : Spherical ) : this
-    // 从球坐标s中设置该向量
-    this.target.position.setFromSpherical(this.spherical);
-    // .lookAt ( eye : Vector3, target : Vector3, up : Vector3 ) : this
-    // 构造一个旋转矩阵，从eye 指向 target，由向量 up 定向
-    this.rotationMatrix.lookAt(this.target.position, this.mesh.position, this.mesh.up);
-    // 四元数（Quaternion）
-    // .setFromRotationMatrix ( m : Matrix4 ) : this
-    // 从m的旋转分量中来设置该四元数
-    // 改编自 here 所概述的方法
-    this.targetQuaternion.setFromRotationMatrix(this.rotationMatrix);
+  private updateMesh() {
+    this.mesh.geometry = this.getGeometry();
+    const material = this.mesh.material as THREE.MeshStandardMaterial
 
-    this.timer && clearTimeout(this.timer);
-    this.timer = setTimeout(() => { this.generateTarget(); }, 2000);
+    let needsUpdate = material.flatShading === this.params.smoothShading;
+    material.flatShading = !this.params.smoothShading;
+
+    if ( this.map !== undefined ) {
+      needsUpdate = needsUpdate || material.map !== (this.params.showMap ? this.map : null);
+      material.map = this.params.showMap ? this.map : null;
+    }
+
+    material.needsUpdate = needsUpdate;
+    this.mesh.material = material;
+  }
+
+  private loadModel() {
+    const loader = new OBJLoader();
+    const url = "/examples/models/obj/cerberus/Cerberus.obj";
+    const toast = showLoadingToast({
+      message: '加载中...',
+      forbidClick: true,
+      loadingType: 'spinner',
+    });
+
+    loader.load(url, (group) => {
+      toast.close();
+      const cerberus = group.children[0] as THREE.Mesh;
+      const modelGeometry = cerberus.geometry;
+
+      this.modifier = new EdgeSplitModifier();
+      this.baseGeometry = BufferGeometryUtils.mergeVertices(modelGeometry);
+
+      this.mesh = new THREE.Mesh(this.getGeometry(), new THREE.MeshStandardMaterial());
+      (this.mesh.material as THREE.MeshStandardMaterial).flatShading = !this.params.smoothShading;
+      this.mesh.rotateY(-Math.PI / 2);
+      this.mesh.scale.set(3.5, 3.5, 3.5);
+      this.mesh.translateZ(1.5);
+      this.scene.add(this.mesh);
+
+      if (this.params.showMap) {
+        (this.mesh.material as THREE.MeshStandardMaterial).map = this.map;
+        (this.mesh.material as THREE.MeshStandardMaterial).needsUpdate = true;
+      }
+    }, undefined, () => { toast.close(); });
+  }
+
+  private loadTexture() {
+    const loader = new THREE.TextureLoader();
+    const url = "/examples/models/obj/cerberus/Cerberus_A.jpg";
+    const toast = showLoadingToast({
+      message: '加载中...',
+      forbidClick: true,
+      loadingType: 'spinner',
+    });
+
+    loader.load(url, (texture) => {
+      toast.close();
+
+      this.map = texture;
+      if (this.params.showMap) {
+        (this.mesh.material as THREE.MeshStandardMaterial).map = this.map;
+        (this.mesh.material as THREE.MeshStandardMaterial).needsUpdate = true;
+      }
+    }, undefined, () => {toast.close();});
+  }
+
+  private getGeometry() {
+    let geometry;
+    if ( this.params.edgeSplit ) {
+      geometry = this.modifier.modify(
+        this.baseGeometry,
+        this.params.cutOffAngle * Math.PI / 180,
+        this.params.tryKeepNormals
+      );
+    } else {
+      geometry = this.baseGeometry;
+    }
+    return geometry;
   }
 
   // 创建渲染器
@@ -152,17 +212,6 @@ export class Model {
   // 持续动画
   private animate() {
     window.requestAnimationFrame(() => { this.animate(); });
-
-    const delta = this.clock.getDelta();
-    // 核心逻辑
-    if (!this.mesh.quaternion.equals(this.targetQuaternion)) {
-      const step = this.speed * delta;
-      // .rotateTowards ( q : Quaternion, step : Float ) : this
-      // q - 目标四元数
-      // step - 以弧度为单位的角度步长
-      // 将该四元数按照步长 step 向目标 q 进行旋转。该方法确保最终的四元数不会超过 q
-      this.mesh.quaternion.rotateTowards(this.targetQuaternion, step);
-    }
 
     this.stats?.update();
     this.controls?.update();
