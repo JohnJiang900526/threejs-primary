72d66f2cbd49278f1e6fb4539434a6d03b7671b6
蒋昂 <jiangang@powerpms.com>
2023-06-08 14:39:54 +0800

    更新文件

---
+++ src/pages/model-list/detail07/197-points-dynamic/tools.ts
@@ -0,0 +1,107 @@
+import * as THREE from 'three';
+import Stats from 'three/examples/jsm/libs/stats.module';
+
+export class Model {
+  private width: number;
+  private height: number;
+  private aspect: number;
+  private container: HTMLDivElement;
+  private scene: THREE.Scene;
+  private renderer: null | THREE.WebGLRenderer;
+  private camera: null | THREE.PerspectiveCamera;
+  private stats: null | Stats;
+
+  constructor(container: HTMLDivElement) {
+    this.container = container;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.aspect = this.width/this.height;
+    this.scene = new THREE.Scene();
+    this.renderer = null;
+    this.camera = null;
+    this.stats = null;
+
+  }
+
+  init() {
+    // 场景
+    this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.FogExp2(0x000000, 0.001);
+
+    // 相机
+    this.camera = new THREE.PerspectiveCamera(55, this.aspect, 2, 2000);
+    this.camera.position.z = 1000;
+
+    // 创建模型
+    this.loadModel();
+
+    // 渲染器
+    this.createRenderer();
+
+    this.initStats();
+    this.animate();
+    this.resize();
+  }
+
+  // 判断是否为移动端
+  isMobile() {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return userAgent.includes("mobile");
+  }
+
+  private loadModel() {
+    
+  }
+
+  // 创建渲染器
+  private createRenderer() {
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.container.appendChild(this.renderer.domElement);
+  }
+
+  // 性能统计
+  private initStats() {
+    // @ts-ignore
+    this.stats = Stats();
+    // @ts-ignore
+    this.stats.domElement.style.position = "absolute";
+    // @ts-ignore
+    this.container.appendChild(this.stats.domElement);
+  }
+
+  // 持续动画
+  private animate() {
+    window.requestAnimationFrame(() => { this.animate(); });
+
+    this.stats?.update();
+    
+    // 执行渲染
+    if (this.renderer && this.camera) {
+      this.renderer.render(this.scene, this.camera);
+    }
+  }
+
+  // 处理自适应
+  resize() {
+    window.onresize = () => {
+      this.width = this.container.offsetWidth;
+      this.height = this.container.offsetHeight;
+      this.aspect = this.width/this.height;
+
+      if (this.camera) {
+        this.camera.aspect = this.aspect;
+        // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
+        this.camera.updateProjectionMatrix();
+      }
+
+      if (this.renderer) {
+        this.renderer.setSize(this.width, this.height);
+      }
+    };
+  }
+}
+
+export default THREE;
+
