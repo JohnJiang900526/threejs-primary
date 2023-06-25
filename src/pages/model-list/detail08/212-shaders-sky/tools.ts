5e1568564dd21f4ba706f575faf9b8036bc94eef
JohnJiang900526 <jiangangshjd@163.com>
2023-06-25 09:21:21 +0800

    完成 212-shaders-sky 案例开发

--- src/pages/model-list/detail08/212-shaders-sky/tools.ts
+++ src/pages/model-list/detail08/212-shaders-sky/tools.ts
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { showLoadingToast } from 'vant';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Sky } from 'three/examples/jsm/objects/Sky';
+import GUI from 'lil-gui';
 
 export class Model {
   private width: number;
@@ -13,11 +14,19 @@ export class Model {
   private camera: null | THREE.PerspectiveCamera;
   private stats: null | Stats;
 
-  private mesh: THREE.Mesh;
-  private mixer: THREE.AnimationMixer | null;
-  private radius: number;
-  private theta: number;
-  private prevTime: number;
+  private controls: null | OrbitControls;
+  private sky: Sky
+  private sun: THREE.Vector3
+  private effectController: {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.7,
+    elevation: 2,
+    azimuth: 180,
+    exposure: number
+  };
+  private gui: GUI
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -28,28 +37,49 @@ export class Model {
     this.camera = null;
     this.stats = null;
 
-    this.mesh = new THREE.Mesh();
-    this.mixer = null;
-    this.radius = 600;
-    this.theta = 0;
-    this.prevTime = Date.now();
+    this.controls = null;
+    this.sky = new Sky();
+    this.sun = new THREE.Vector3();
+    this.effectController = {
+      turbidity: 10,
+      rayleigh: 3,
+      mieCoefficient: 0.005,
+      mieDirectionalG: 0.7,
+      elevation: 2,
+      azimuth: 180,
+      exposure: 0.5
+    };
+    this.gui = new GUI({
+      container: this.container,
+      autoPlace: false,
+      title: "控制器"
+    });
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xf0f0f0);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 10000);
-    this.camera.position.y = 300;
+    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 100, 2000000);
+    this.camera.position.set(0, 100, 2000);
 
-    this.generateLight();
-    this.loadModel();
     // 渲染器
     this.createRenderer();
+    // help
+    this.createHelp();
+    // sky
+    this.initSky();
+    // set value
+    this.guiChanged();
+
+    // 控制器
+    this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
+    this.controls.enableZoom = false;
+    this.controls.enablePan = false;
 
     this.initStats();
+    this.setUpGUI();
     this.animate();
     this.resize();
   }
@@ -60,40 +90,63 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
-  private loadModel() {
-    const loader = new GLTFLoader();
-    const url = "/examples/models/gltf/Horse.glb";
-    const toast = showLoadingToast({
-      message: '加载中...',
-      forbidClick: true,
-      loadingType: 'spinner',
+  private setUpGUI() {
+    this.gui.add(this.effectController, 'turbidity', 0.0, 20.0, 0.1).name("浊度").onChange(() => {
+      this.guiChanged();
     });
-    loader.load(url, (gltf) => {
-      toast.close();
-
-      // 核心
-      this.mesh = gltf.scene.children[0] as THREE.Mesh;
-      this.mesh.scale.set(1.5, 1.5, 1.5);
-      this.scene.add(this.mesh);
-      this.mixer = new THREE.AnimationMixer(this.mesh);
-      this.mixer.clipAction(gltf.animations[0]).setDuration(1).play();
-    }, undefined, () => { toast.close(); })
+    this.gui.add(this.effectController, 'rayleigh', 0.0, 4, 0.001).name("瑞利").onChange(() => {
+      this.guiChanged();
+    });
+    this.gui.add(this.effectController, 'mieCoefficient', 0.0, 0.1, 0.001).name("mie系数").onChange(() => {
+      this.guiChanged();
+    });
+    this.gui.add(this.effectController, 'mieDirectionalG', 0.0, 1, 0.001).name("mie方向G").onChange(() => {
+      this.guiChanged();
+    });
+    this.gui.add(this.effectController, 'elevation', 0, 30, 0.1).name("高度").onChange(() => {
+      this.guiChanged();
+    });
+    this.gui.add(this.effectController, 'azimuth', -180, 180, 0.1).name("方位角").onChange(() => {
+      this.guiChanged();
+    });
+    this.gui.add(this.effectController, 'exposure', 0, 1, 0.0001).name("亮度").onChange(() => {
+      this.guiChanged();
+    });
   }
 
-  private generateLight() {
-    const light1 = new THREE.DirectionalLight(0xefefff, 1.5);
-    light1.position.set(1, 1, 1).normalize();
+  private guiChanged() {
+    const uniforms = this.sky.material.uniforms;
+    uniforms['turbidity'].value = this.effectController.turbidity;
+    uniforms['rayleigh'].value = this.effectController.rayleigh;
+    uniforms['mieCoefficient'].value = this.effectController.mieCoefficient;
+    uniforms['mieDirectionalG'].value = this.effectController.mieDirectionalG;
 
-    const light2 = new THREE.DirectionalLight(0xffefef, 1.5);
-    light2.position.set(-1, -1, -1).normalize();
-    
-    this.scene.add(light1, light2);
+    const phi = THREE.MathUtils.degToRad(90 - this.effectController.elevation);
+    const theta = THREE.MathUtils.degToRad(this.effectController.azimuth);
+
+    this.sun.setFromSphericalCoords(1, phi, theta);
+    uniforms['sunPosition'].value.copy(this.sun);
+    (this.renderer as THREE.WebGLRenderer).toneMappingExposure = this.effectController.exposure;
+  }
+
+  private initSky() {
+    this.sky.scale.setScalar(450000);
+		this.scene.add(this.sky);
+
+    this.effectController.exposure = (this.renderer as THREE.WebGLRenderer).toneMappingExposure;
+  }
+
+  private createHelp() {
+    const helper = new THREE.GridHelper(10000, 2, 0xffffff, 0xffffff);
+    this.scene.add(helper);
   }
 
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 0.5;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
@@ -112,18 +165,6 @@ export class Model {
   // 持续动画
   private animate() {
     window.requestAnimationFrame(() => { this.animate(); });
-
-    // 核心
-    if (this.camera) {
-      this.theta += 0.1;
-      this.camera.position.x = this.radius * Math.sin(THREE.MathUtils.degToRad(this.theta));
-      this.camera.position.z = this.radius * Math.cos(THREE.MathUtils.degToRad(this.theta));
-      this.camera.lookAt(0, 150, 0);
-
-      const time = Date.now();
-      this.mixer?.update((time - this.prevTime) * 0.001);
-      this.prevTime = time;
-    }
 
     this.stats?.update();
     
