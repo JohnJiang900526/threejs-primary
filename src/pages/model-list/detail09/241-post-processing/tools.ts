388ed93cbb26707dfe331f7cc50f2bad009c8492
JohnJiang900526 <jiangangshjd@163.com>
2023-07-05 09:29:41 +0800

    完成 241-post-processing 案例开发

--- src/pages/model-list/detail09/241-post-processing/tools.ts
+++ src/pages/model-list/detail09/241-post-processing/tools.ts
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader';
+import { DotScreenShader } from 'three/examples/jsm/shaders/DotScreenShader';
 
 export class Model {
   private width: number;
@@ -14,6 +19,8 @@ export class Model {
   private animateNumber: number;
 
   private controls: null | OrbitControls;
+  private composer: null | EffectComposer;
+  private object: THREE.Object3D;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -26,20 +33,28 @@ export class Model {
     this.animateNumber = 0;
 
     this.controls = null;
+    this.composer = null;
+    this.object = new THREE.Object3D();
   }
 
   init() {
-    // 场景
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xaec5d5);
-
-    // 相机
-    this.camera = new THREE.PerspectiveCamera(27, this.aspect, 1, 10000);
-    this.camera.position.z = 2000;
-
     // 渲染器
     this.createRenderer();
 
+    // 场景
+    this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.Fog(0x000000, 1, 1000);
+    this.scene.add(this.object);
+
+    // 相机
+    this.camera = new THREE.PerspectiveCamera(70, this.aspect, 1, 1000);
+    this.camera.position.z = 400;
+
+    this.createLight();
+    this.createMesh();
+    this.initComposer();
+
+    // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.update();
 
@@ -52,6 +67,50 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private initComposer() {
+    if (this.renderer && this.camera) {
+      this.composer = new EffectComposer(this.renderer);
+      this.composer.addPass(new RenderPass(this.scene, this.camera));
+  
+      const effect1 = new ShaderPass(DotScreenShader);
+      effect1.uniforms['scale'].value = 4;
+      this.composer.addPass(effect1);
+  
+      const effect2 = new ShaderPass(RGBShiftShader);
+      effect2.uniforms['amount'].value = 0.0015;
+      this.composer.addPass(effect2);
+    }
+  }
+
+  private createMesh() {
+    const geometry = new THREE.SphereGeometry(1, 4, 4);
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+
+    for (let i = 0; i < 100; i++) {
+      const mesh = new THREE.Mesh(geometry, material);
+      const scale = Math.random() * 50;
+
+      const x = Math.random() - 0.5;
+      const y = Math.random() - 0.5;
+      const z = Math.random() - 0.5;
+
+      mesh.position.set(x, y, z).normalize();
+      mesh.position.multiplyScalar(Math.random() * 400);
+      mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+      mesh.scale.set(scale, scale, scale);
+      this.object.add(mesh);
+    }
+  }
+
+  private createLight() {
+    const ambient = new THREE.AmbientLight(0x222222);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(1, 1, 1);
+    
+    this.scene.add(ambient, dirLight);
   }
 
   // 创建渲染器
@@ -77,13 +136,14 @@ export class Model {
     this.animateNumber && window.cancelAnimationFrame(this.animateNumber);
     this.animateNumber = window.requestAnimationFrame(() => { this.animate(); });
 
+    {
+      this.object.rotation.x += 0.005;
+      this.object.rotation.y += 0.01;
+    }
     this.stats?.update();
     this.controls?.update();
 
-    // 执行渲染
-    if (this.renderer && this.camera) {
-      this.renderer.render(this.scene, this.camera);
-    }
+    this.composer?.render();
   }
 
   // 消除 副作用
@@ -106,6 +166,10 @@ export class Model {
 
       if (this.renderer) {
         this.renderer.setSize(this.width, this.height);
+      }
+
+      if (this.composer) {
+        this.composer.setSize(this.width, this.height);
       }
     };
   }
