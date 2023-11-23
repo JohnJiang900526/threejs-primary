0a8a9188a64d4a1dffba01f77dc16b5960c2ea04
JohnJiang900526 <jiangangshjd@163.com>
2023-11-23 13:44:16 +0800

    完成 260-post-processing-sobel 案例开发

--- src/pages/model-list/detail09/260-post-processing-sobel/tools.ts
+++ src/pages/model-list/detail09/260-post-processing-sobel/tools.ts
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader';
+import { SobelOperatorShader } from 'three/examples/jsm/shaders/SobelOperatorShader';
+import GUI from 'lil-gui';
 
 export class Model {
   private width: number;
@@ -14,6 +20,12 @@ export class Model {
   private animateNumber: number;
 
   private controls: null | OrbitControls;
+  private composer: null | EffectComposer;
+  private effectSobel: null | ShaderPass;
+  private params: {
+    enable: boolean
+  };
+  private gui: GUI;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -26,23 +38,44 @@ export class Model {
     this.animateNumber = 0;
 
     this.controls = null;
+    this.composer = null;
+    this.effectSobel = null;
+    this.params = {
+      enable: true
+    };
+    this.gui = new GUI({
+      title: "控制面板",
+      autoPlace: false,
+      container: this.container,
+    });
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xaec5d5);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(27, this.aspect, 1, 10000);
-    this.camera.position.z = 2000;
+    this.camera = new THREE.PerspectiveCamera(70, this.aspect, 0.1, 200);
+    this.camera.position.set(0, 10, 25);
+    this.camera.lookAt(this.scene.position);
+    this.scene.add(this.camera);
 
+    // 灯光
+    this.generateLight();
+    // 模型
+    this.generateMesh();
     // 渲染器
     this.createRenderer();
+    // 效果合成器
+    this.initComposer();
 
+    // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
+    this.controls.minDistance = 10;
+    this.controls.maxDistance = 100;
     this.controls.update();
 
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -54,12 +87,58 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  private setGUI() {
+    this.gui.add(this.params, "enable").name("是否使用合成器");
+  }
+
+  private generateLight() {
+    const light1 = new THREE.AmbientLight(0xcccccc, 0.4);
+    const light2 = new THREE.PointLight(0xffffff, 0.8);
+
+    this.scene.add(light1, light2);
+  }
+
+  private generateMesh() {
+    // 圆环缓冲扭结几何体（TorusKnotGeometry）
+    // TorusKnotGeometry(radius : Float, tube : Float, tubularSegments : Integer, radialSegments : Integer, p : Integer, q : Integer)
+    // radius - 圆环的半径，默认值为1。
+    // tube — 管道的半径，默认值为0.4。
+    // tubularSegments — 管道的分段数量，默认值为64。
+    // radialSegments — 横截面分段数量，默认值为8。
+    // p — 这个值决定了几何体将绕着其旋转对称轴旋转多少次，默认值是2。
+    // q — 这个值决定了几何体将绕着其内部圆环旋转多少次，默认值是3。
+    const geometry = new THREE.TorusKnotGeometry(4, 1, 256, 32, 2, 3);
+    const material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.name = "torus";
+    this.scene.add(mesh);
+  }
+
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
+  }
+
+  // 效果合成器
+  private initComposer() {
+    this.composer = new EffectComposer(this.renderer!);
+
+    const renderPass = new RenderPass(this.scene, this.camera!);
+    this.composer.addPass(renderPass);
+
+    const effectGrayScale = new ShaderPass(LuminosityShader);
+    this.composer.addPass(effectGrayScale);
+
+    const width = this.width * window.devicePixelRatio;
+    const height = this.height * window.devicePixelRatio;
+    this.effectSobel = new ShaderPass(SobelOperatorShader);
+    this.effectSobel.uniforms['resolution'].value.x = width;
+    this.effectSobel.uniforms['resolution'].value.y = height;
+    this.composer.addPass(this.effectSobel);
   }
 
   // 性能统计
@@ -80,9 +159,19 @@ export class Model {
     this.stats?.update();
     this.controls?.update();
 
+    const timer = performance.now();
+    const mesh = this.scene.getObjectByName("torus");
+    mesh!.rotation.set(
+      timer * 0.0005,
+      timer * 0.0005,
+      timer * 0.0005,
+    );
+
     // 执行渲染
-    if (this.renderer && this.camera) {
-      this.renderer.render(this.scene, this.camera);
+    if (this.params.enable) {
+      this.composer!.render();
+    } else {
+      this.renderer!.render(this.scene, this.camera!);
     }
   }
 
@@ -104,9 +193,13 @@ export class Model {
         this.camera.updateProjectionMatrix();
       }
 
-      if (this.renderer) {
-        this.renderer.setSize(this.width, this.height);
-      }
+      this.renderer?.setSize(this.width, this.height);
+      this.composer?.setSize(this.width, this.height);
+
+      const width = this.width * window.devicePixelRatio;
+      const height = this.height * window.devicePixelRatio;
+      this.effectSobel!.uniforms['resolution'].value.x = width;
+      this.effectSobel!.uniforms['resolution'].value.y = height;
     };
   }
 }
