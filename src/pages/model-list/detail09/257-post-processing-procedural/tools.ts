62e2dbb1d2e88adeb0846033c2d266958df8ce10
JohnJiang900526 <jiangangshjd@163.com>
2023-11-23 10:26:00 +0800

    完成 257-post-processing-procedural 案例开发

--- src/pages/model-list/detail09/257-post-processing-procedural/tools.ts
+++ src/pages/model-list/detail09/257-post-processing-procedural/tools.ts
@@ -1,48 +1,113 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export class Model {
   private width: number;
   private height: number;
-  private aspect: number;
   private container: HTMLDivElement;
   private scene: THREE.Scene;
   private renderer: null | THREE.WebGLRenderer;
-  private camera: null | THREE.PerspectiveCamera;
+  private camera: null | THREE.OrthographicCamera;
   private stats: null | Stats;
   private animateNumber: number;
 
-  private controls: null | OrbitControls;
+  private proceduralVert: string;
+  private noiseRandom1DFrag: string;
+  private noiseRandom2DFrag: string;
+  private noiseRandom3DFrag: string;
+  private postMaterial: THREE.ShaderMaterial
+  private noiseRandom1DMaterial: THREE.ShaderMaterial;
+  private noiseRandom2DMaterial: THREE.ShaderMaterial;
+  private noiseRandom3DMaterial: THREE.ShaderMaterial;
+  private postQuad: THREE.Mesh<THREE.PlaneGeometry, THREE.ShaderMaterial>;
+  private params: { 
+    procedure: string
+  };
+  private gui: GUI;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
-    this.aspect = this.width/this.height;
     this.scene = new THREE.Scene();
     this.renderer = null;
     this.camera = null;
     this.stats = null;
     this.animateNumber = 0;
 
-    this.controls = null;
+    this.proceduralVert = `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `;
+    this.noiseRandom1DFrag = `
+      #include <common>
+      varying vec2 vUv;
+      void main() {
+        gl_FragColor.xyz = vec3( rand( vUv ) );
+        gl_FragColor.w = 1.0;
+      }
+    `;
+    this.noiseRandom2DFrag = `
+      #include <common>
+      varying vec2 vUv;
+      void main() {
+        vec2 rand2 = vec2( rand( vUv ), rand( vUv + vec2( 0.4, 0.6 ) ) );
+        gl_FragColor.xyz = mix( mix( vec3( 1.0, 1.0, 1.0 ), vec3( 0.0, 0.0, 1.0 ), rand2.x ), vec3( 0.0 ), rand2.y );
+        gl_FragColor.w = 1.0;
+      }
+    `;
+    this.noiseRandom3DFrag = `
+      #include <common>
+
+      varying vec2 vUv;
+      void main() {
+        vec3 rand3 = vec3( rand( vUv ), rand( vUv + vec2( 0.4, 0.6 ) ), rand( vUv + vec2( 0.6, 0.4 ) ) );
+        gl_FragColor.xyz = rand3;
+        gl_FragColor.w = 1.0;
+      }
+    `;
+
+    this.params = {
+      procedure: 'noiseRandom3D'
+    };
+
+    this.noiseRandom1DMaterial = new THREE.ShaderMaterial({
+      vertexShader: this.proceduralVert,
+      fragmentShader: this.noiseRandom1DFrag,
+    });
+    this.noiseRandom2DMaterial = new THREE.ShaderMaterial({
+      vertexShader: this.proceduralVert,
+      fragmentShader: this.noiseRandom2DFrag,
+    });
+    this.noiseRandom3DMaterial = new THREE.ShaderMaterial({
+      vertexShader: this.proceduralVert,
+      fragmentShader: this.noiseRandom3DFrag,
+    });
+    this.postMaterial = this.noiseRandom3DMaterial;
+    this.postQuad = new THREE.Mesh(new THREE.PlaneGeometry( 2, 2), this.postMaterial);
+    this.gui = new GUI({
+      title: "控制面板",
+      autoPlace: false,
+      container: this.container,
+    });
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xaec5d5);
+    this.scene.add(this.postQuad);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(27, this.aspect, 1, 10000);
-    this.camera.position.z = 2000;
+    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
     // 渲染器
     this.createRenderer();
 
-    this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
-    this.controls.update();
-
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -52,6 +117,11 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private setGUI() {
+    const values = ['noiseRandom1D', 'noiseRandom2D', 'noiseRandom3D'];
+    this.gui.add(this.params, "procedure", values);
   }
 
   // 创建渲染器
@@ -78,12 +148,22 @@ export class Model {
     this.animateNumber = window.requestAnimationFrame(() => { this.animate(); });
 
     this.stats?.update();
-    this.controls?.update();
 
-    // 执行渲染
-    if (this.renderer && this.camera) {
-      this.renderer.render(this.scene, this.camera);
+    switch (this.params.procedure) {
+      case 'noiseRandom1D': 
+        this.postMaterial = this.noiseRandom1DMaterial; 
+        break;
+      case 'noiseRandom2D': 
+        this.postMaterial = this.noiseRandom2DMaterial; 
+        break;
+      case 'noiseRandom3D': 
+        this.postMaterial = this.noiseRandom3DMaterial; 
+        break;
     }
+
+    this.postQuad.material = this.postMaterial;
+    // 执行渲染
+    this.renderer!.render(this.scene, this.camera!);
   }
 
   // 消除 副作用
@@ -96,17 +176,9 @@ export class Model {
     window.onresize = () => {
       this.width = this.container.offsetWidth;
       this.height = this.container.offsetHeight;
-      this.aspect = this.width/this.height;
 
-      if (this.camera) {
-        this.camera.aspect = this.aspect;
-        // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
-        this.camera.updateProjectionMatrix();
-      }
-
-      if (this.renderer) {
-        this.renderer.setSize(this.width, this.height);
-      }
+      this.camera?.updateProjectionMatrix();
+      this.renderer?.setSize(this.width, this.height);
     };
   }
 }
