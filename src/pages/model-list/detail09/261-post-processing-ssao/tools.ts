b6f968389e60ef8718ffce73495e2a24128e9820
JohnJiang900526 <jiangangshjd@163.com>
2023-11-24 10:12:32 +0800

    完成 261-post-processing-ssao 案例开发

--- src/pages/model-list/detail09/261-post-processing-ssao/tools.ts
+++ src/pages/model-list/detail09/261-post-processing-ssao/tools.ts
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
+import GUI from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass';
 
 export class Model {
   private width: number;
@@ -14,6 +17,13 @@ export class Model {
   private animateNumber: number;
 
   private controls: null | OrbitControls;
+  private composer: null | EffectComposer;
+  private group: THREE.Group;
+  private gui: GUI;
+  private ssaoPass: null | SSAOPass;
+  private params: {
+    enable: boolean
+  }
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -26,22 +36,43 @@ export class Model {
     this.animateNumber = 0;
 
     this.controls = null;
+    this.composer = null;
+    this.group = new THREE.Group();
+    this.gui = new GUI({
+      title: "控制面板",
+      autoPlace: false,
+      container: this.container,
+    });
+    this.ssaoPass = null;
+    this.params = {
+      enable: true
+    };
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
+    this.scene.add(this.group);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(27, this.aspect, 1, 10000);
-    this.camera.position.z = 2000;
+    this.camera = new THREE.PerspectiveCamera(85, this.aspect, 1, 2000);
+    this.camera.position.z = 500;
 
+    // 灯光
+    this.generateLight();
+    // 创建模型
+    this.generateMesh();
     // 渲染器
     this.createRenderer();
+    // 效果合成器
+    this.initComposer();
 
+    // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.update();
 
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -53,12 +84,74 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  private setGUI() {
+    const OUTPUT: any = SSAOPass.OUTPUT;
+
+    const values = {
+      'Default': OUTPUT.Default,
+      'SSAO Only': OUTPUT.SSAO,
+      'SSAO Only + Blur': OUTPUT.Blur,
+      'Beauty': OUTPUT.Beauty,
+      'Depth': OUTPUT.Depth,
+      'Normal': OUTPUT.Normal
+    };
+
+    this.gui.add(this.params, 'enable').name("启用合成器");
+    this.gui.add(this.ssaoPass!, 'output', values).onChange((value: string) => {
+      this.ssaoPass!.output = parseInt(value);
+    });
+    this.gui.add(this.ssaoPass!, 'kernelRadius', 0, 32).min( 0 ).max( 32 );
+    this.gui.add(this.ssaoPass!, 'minDistance', 0.001, 0.02).min( 0.001 ).max( 0.02 );
+    this.gui.add(this.ssaoPass!, 'maxDistance', 0.01, 0.3).min( 0.01 ).max( 0.3 );
+  }
+
+  private generateMesh() {
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+
+    for (let i = 0; i < 100; i++) {
+      const material = new THREE.MeshLambertMaterial({
+        color: Math.random() * 0xffffff
+      });
+
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.set(
+        Math.random() * 400 - 200,
+        Math.random() * 400 - 200,
+        Math.random() * 400 - 200,
+      );
+      mesh.rotation.set(
+        Math.random(), 
+        Math.random(), 
+        Math.random(),
+      );
+
+      mesh.scale.setScalar(Math.random() * 10 + 2);
+      this.group.add(mesh);
+    }
+  }
+
+  private generateLight() {
+    const light1 = new THREE.DirectionalLight();
+    const light2 = new THREE.HemisphereLight();
+
+    this.scene.add(light1, light2);
+  }
+
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
+  }
+
+  // 初始化效果合成器
+  private initComposer() {
+    this.composer = new EffectComposer(this.renderer!);
+
+    this.ssaoPass = new SSAOPass(this.scene, this.camera!, this.width, this.height);
+    this.ssaoPass.kernelRadius = 16;
+    this.composer.addPass(this.ssaoPass);
   }
 
   // 性能统计
@@ -79,9 +172,18 @@ export class Model {
     this.stats?.update();
     this.controls?.update();
 
+    {
+      // 控制旋转
+      const timer = performance.now();
+      this.group.rotation.x = timer * 0.0002;
+      this.group.rotation.y = timer * 0.0001;
+    }
+
     // 执行渲染
-    if (this.renderer && this.camera) {
-      this.renderer.render(this.scene, this.camera);
+    if (this.params.enable) {
+      this.composer?.render();
+    } else {
+      this.renderer?.render(this.scene, this.camera!);
     }
   }
 
@@ -97,15 +199,12 @@ export class Model {
       this.height = this.container.offsetHeight;
       this.aspect = this.width/this.height;
 
-      if (this.camera) {
-        this.camera.aspect = this.aspect;
-        // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
-        this.camera.updateProjectionMatrix();
-      }
+      this.camera!.aspect = this.aspect;
+      // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
+      this.camera?.updateProjectionMatrix();
 
-      if (this.renderer) {
-        this.renderer.setSize(this.width, this.height);
-      }
+      this.renderer?.setSize(this.width, this.height);
+      this.composer?.setSize(this.width, this.height);
     };
   }
 }
