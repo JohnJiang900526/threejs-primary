d5b98c632e84357e5ee2d465e88abc13f6df9018
JohnJiang900526 <jiangangshjd@163.com>
2023-11-28 14:37:23 +0800

    完成 270-buffer-geometry-glbufferattribute 案例开发

--- src/pages/model-list/detail09/270-buffer-geometry-glbufferattribute/tools.ts
+++ src/pages/model-list/detail09/270-buffer-geometry-glbufferattribute/tools.ts
@@ -14,6 +14,9 @@ export class Model {
   private animateNumber: number;
 
   private controls: null | OrbitControls;
+  private points: null | THREE.Points;
+  private particles: number; 
+  private drawCount: number;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -26,19 +29,27 @@ export class Model {
     this.animateNumber = 0;
 
     this.controls = null;
+    this.points = null;
+    this.particles = 300000;
+    this.drawCount = 10000;
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x050505);
+    this.scene.fog = new THREE.Fog(0x050505, 2000, 3500);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(27, this.aspect, 1, 10000);
-    this.camera.position.z = 2000;
+    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 0.01, 3500);
+    this.camera.position.z = 1750;
 
     // 渲染器
     this.createRenderer();
-
+    // 创建模型
+    this.generateModel();
+    
+    // 控制面板
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.update();
 
@@ -51,6 +62,63 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private generateModel() {
+    const geometry = new THREE.BufferGeometry();
+    const positions = [];
+    const positions2 = [];
+    const colors = [];
+
+    const color = new THREE.Color();
+    const n = 1000, n2 = n / 2;
+
+    for (let i = 0; i < this.particles; i++) {
+      // 位置
+      const x = Math.random() * n - n2;
+      const y = Math.random() * n - n2;
+      const z = Math.random() * n - n2;
+
+      positions.push(x, y, z);
+      positions2.push(z * 0.5, x * 0.5, y * 0.5);
+
+      // 颜色
+      color.setRGB((x / n) + 0.5, (y / n) + 0.5, (z / n) + 0.5);
+      colors.push(color.r, color.g, color.b);
+    }
+
+    const gl = this.renderer!.getContext();
+    const pos = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pos);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    const pos2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pos2);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions2), gl.STATIC_DRAW);
+
+    const rgb = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, rgb);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const posAttr1 = new THREE.GLBufferAttribute(pos!, gl.FLOAT, 3, 4, this.particles);
+    const posAttr2 = new THREE.GLBufferAttribute(pos2!, gl.FLOAT, 3, 4, this.particles);
+
+    geometry.setAttribute('position', posAttr1);
+    geometry.setAttribute('color', new THREE.GLBufferAttribute(rgb!, gl.FLOAT, 3, 4, this.particles));
+
+    setInterval(() => {
+      const position = geometry.getAttribute('position');
+      geometry.setAttribute('position', (position === posAttr1) ? posAttr2 : posAttr1);
+    }, 2000);
+
+    const material = new THREE.PointsMaterial({ 
+      size: 15, 
+      vertexColors: true,
+    });
+    this.points = new THREE.Points(geometry, material);
+    // geometry.boundingSphere = (new THREE.Sphere()).set(new THREE.Vector3(), Infinity);
+    this.points.frustumCulled = false;
+    this.scene.add(this.points);
   }
 
   // 创建渲染器
@@ -79,10 +147,19 @@ export class Model {
     this.stats?.update();
     this.controls?.update();
 
-    // 执行渲染
-    if (this.renderer && this.camera) {
-      this.renderer.render(this.scene, this.camera);
+    {
+      const timer = Date.now() * 0.001;
+      this.drawCount = (Math.max(5000, this.drawCount) + Math.floor(500 * Math.random())) % this.particles;
+      
+      if (this.points) {
+        this.points.geometry.setDrawRange(0, this.drawCount);
+        this.points.rotation.x = timer * 0.1;
+        this.points.rotation.y = timer * 0.2;
+      }
     }
+
+    // 执行渲染
+    this.renderer!.render(this.scene, this.camera!);
   }
 
   // 消除 副作用
@@ -97,15 +174,10 @@ export class Model {
       this.height = this.container.offsetHeight;
       this.aspect = this.width/this.height;
 
-      if (this.camera) {
-        this.camera.aspect = this.aspect;
-        // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
-        this.camera.updateProjectionMatrix();
-      }
-
-      if (this.renderer) {
-        this.renderer.setSize(this.width, this.height);
-      }
+      this.camera!.aspect = this.aspect;
+      // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
+      this.camera!.updateProjectionMatrix();
+      this.renderer!.setSize(this.width, this.height);
     };
   }
 }
