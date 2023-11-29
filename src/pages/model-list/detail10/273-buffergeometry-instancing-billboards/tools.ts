42e194f8ebda10083978e99592cec400ee2ad5e1
JohnJiang900526 <jiangangshjd@163.com>
2023-11-29 13:55:23 +0800

    完成 273-buffergeometry-instancing-billboards 案例开发和解析

--- src/pages/model-list/detail10/273-buffergeometry-instancing-billboards/tools.ts
+++ src/pages/model-list/detail10/273-buffergeometry-instancing-billboards/tools.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GUI from 'lil-gui';
 
 export class Model {
   private width: number;
@@ -16,6 +16,11 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private geometry: THREE.InstancedBufferGeometry;
+  private material: THREE.RawShaderMaterial;
+  private mesh: THREE.Mesh;
+  private vertexShader: string;
+  private fragmentShader: string;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,6 +38,64 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.gui.hide();
+    this.geometry = new THREE.InstancedBufferGeometry();
+    this.material = new THREE.RawShaderMaterial({});
+    this.mesh = new THREE.Mesh();
+    this.vertexShader = `
+      precision highp float;
+      uniform mat4 modelViewMatrix;
+      uniform mat4 projectionMatrix;
+      uniform float time;
+
+      attribute vec3 position;
+      attribute vec2 uv;
+      attribute vec3 translate;
+
+      varying vec2 vUv;
+      varying float vScale;
+
+      void main() {
+        vec4 mvPosition = modelViewMatrix * vec4( translate, 1.0 );
+        vec3 trTime = vec3(translate.x + time,translate.y + time,translate.z + time);
+        float scale =  sin( trTime.x * 2.1 ) + sin( trTime.y * 3.2 ) + sin( trTime.z * 4.3 );
+        vScale = scale;
+        scale = scale * 10.0 + 10.0;
+        mvPosition.xyz += position * scale;
+        vUv = uv;
+        gl_Position = projectionMatrix * mvPosition;
+      }
+    `;
+    this.fragmentShader = `
+      precision highp float;
+
+      uniform sampler2D map;
+
+      varying vec2 vUv;
+      varying float vScale;
+
+      // HSL to RGB Convertion helpers
+      vec3 HUEtoRGB(float H){
+        H = mod(H,1.0);
+        float R = abs(H * 6.0 - 3.0) - 1.0;
+        float G = 2.0 - abs(H * 6.0 - 2.0);
+        float B = 2.0 - abs(H * 6.0 - 4.0);
+        return clamp(vec3(R,G,B),0.0,1.0);
+      }
+
+      vec3 HSLtoRGB(vec3 HSL){
+        vec3 RGB = HUEtoRGB(HSL.x);
+        float C = (1.0 - abs(2.0 * HSL.z - 1.0)) * HSL.y;
+        return (RGB - 0.5) * C + HSL.z;
+      }
+
+      void main() {
+        vec4 diffuseColor = texture2D( map, vUv );
+        gl_FragColor = vec4( diffuseColor.xyz * HSLtoRGB(vec3(vScale/5.0, 1.0, 0.5)), diffuseColor.w );
+
+        if ( diffuseColor.w < 0.5 ) discard;
+      }
+    `;
   }
 
   init() {
@@ -40,9 +103,11 @@ export class Model {
     this.scene = new THREE.Scene();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
-    this.camera.position.z = 2750;
+    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 1, 5000);
+    this.camera.position.z = 2000;
 
+    // 模型
+    this.generateMesh();
     // 渲染器
     this.createRenderer();
 
@@ -59,6 +124,46 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  // 核心
+  private generateMesh() {
+    const scale = 500;
+    // 圆形缓冲几何体（CircleGeometry）
+    const circleGeometry = new THREE.CircleGeometry(1, 6);
+
+    this.geometry = new THREE.InstancedBufferGeometry();
+    this.geometry.index = circleGeometry.index;
+    this.geometry.attributes = circleGeometry.attributes;
+
+    const particleCount = 75000;
+    const translateArray = new Float32Array(particleCount * 3);
+
+    for (let i = 0, i3 = 0; i < particleCount; i++, i3 += 3) {
+      translateArray[i3 + 0] = Math.random() * 2 - 1;
+      translateArray[i3 + 1] = Math.random() * 2 - 1;
+      translateArray[i3 + 2] = Math.random() * 2 - 1;
+    }
+
+    const translateAttr = new THREE.InstancedBufferAttribute(translateArray, 3);
+    this.geometry.setAttribute('translate', translateAttr);
+
+    const loader = new THREE.TextureLoader();
+    const map = loader.load("/examples/textures/sprites/circle.png");
+    this.material = new THREE.RawShaderMaterial({
+      depthTest: true,
+      depthWrite: true,
+      uniforms: {
+        'map': { value: map },
+        'time': { value: 0.0 },
+      },
+      vertexShader: this.vertexShader,
+      fragmentShader: this.fragmentShader,
+    });
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.scale.set(scale, scale, scale);
+    this.scene.add(this.mesh);
   }
 
   // 创建渲染器
@@ -86,6 +191,13 @@ export class Model {
 
     this.stats?.update();
     this.controls?.update();
+
+    {
+      const timer = performance.now() * 0.0005;
+			this.material.uniforms['time'].value = timer;
+			this.mesh.rotation.x = timer * 0.2;
+			this.mesh.rotation.y = timer * 0.4;
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
