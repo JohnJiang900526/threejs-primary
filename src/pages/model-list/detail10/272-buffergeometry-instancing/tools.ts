5a5dcb9e36657ea3f22507ba361234542d64ed93
JohnJiang900526 <jiangangshjd@163.com>
2023-11-29 11:32:31 +0800

    完成 272-buffergeometry-instancing 案例开发

--- src/pages/model-list/detail10/272-buffergeometry-instancing/tools.ts
+++ src/pages/model-list/detail10/272-buffergeometry-instancing/tools.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import GUI from 'lil-gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export class Model {
   private width: number;
@@ -16,6 +16,10 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private geometry: THREE.InstancedBufferGeometry;
+  private vertexShader: string;
+  private fragmentShader: string;
+  private instances: number;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,6 +37,49 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.geometry = new THREE.InstancedBufferGeometry();
+    this.vertexShader = `
+      precision highp float;
+
+      uniform float sineTime;
+      uniform mat4 modelViewMatrix;
+      uniform mat4 projectionMatrix;
+
+      attribute vec3 position;
+      attribute vec3 offset;
+      attribute vec4 color;
+      attribute vec4 orientationStart;
+      attribute vec4 orientationEnd;
+
+      varying vec3 vPosition;
+      varying vec4 vColor;
+
+      void main(){
+        vPosition = offset * max( abs( sineTime * 2.0 + 1.0 ), 0.5 ) + position;
+        vec4 orientation = normalize( mix( orientationStart, orientationEnd, sineTime ) );
+        vec3 vcV = cross( orientation.xyz, vPosition );
+        vPosition = vcV * ( 2.0 * orientation.w ) + ( cross( orientation.xyz, vcV ) * 2.0 + vPosition );
+
+        vColor = color;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
+      }
+    `;
+    this.fragmentShader = `
+      precision highp float;
+
+      uniform float time;
+      
+      varying vec3 vPosition;
+      varying vec4 vColor;
+
+      void main() {
+        vec4 color = vec4( vColor );
+        color.r += sin( vPosition.x * 10.0 + time ) * 0.5;
+
+        gl_FragColor = color;
+      }
+    `;
+    this.instances = 5000;
   }
 
   init() {
@@ -40,9 +87,11 @@ export class Model {
     this.scene = new THREE.Scene();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
-    this.camera.position.z = 2750;
+    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 0.01, 100);
+    this.camera.position.z = 2;
 
+    // 模型
+    this.generateMesh();
     // 渲染器
     this.createRenderer();
 
@@ -50,6 +99,7 @@ export class Model {
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.update();
 
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -59,6 +109,86 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private setGUI() {
+    this.gui.add(this.geometry, 'instanceCount', 0, this.instances);
+  }
+
+  // 核心模块
+  private generateMesh() {
+    const vector = new THREE.Vector4();
+
+    const positions = [];
+    const offsets = [];
+    const colors = [];
+    const orientationsStart = [];
+    const orientationsEnd = [];
+
+    positions.push(0.025, -0.025, 0);
+    positions.push(-0.025, 0.025, 0);
+    positions.push(0, 0, 0.025);
+
+    for (let i = 0; i < this.instances; i++) {
+      // 偏移量
+      offsets.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+      // 颜色
+      colors.push(Math.random(), Math.random(), Math.random(), Math.random());
+
+      // orientationStart 朝向 开始
+      vector.set(
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1,
+      );
+      vector.normalize();
+      orientationsStart.push(vector.x, vector.y, vector.z, vector.w);
+
+      // orientationEnd 朝向 结束
+      vector.set(
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1, 
+        Math.random() * 2 - 1,
+      );
+      vector.normalize();
+      orientationsEnd.push(vector.x, vector.y, vector.z, vector.w);
+    }
+
+    this.geometry.dispose();
+    this.geometry = new THREE.InstancedBufferGeometry();
+    this.geometry.instanceCount = this.instances;
+
+    const positionAttr = new THREE.Float32BufferAttribute(positions, 3);
+    this.geometry.setAttribute('position', positionAttr);
+
+    const offsetAttr = new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3);
+    this.geometry.setAttribute('offset', offsetAttr);
+
+    const colorAttr = new THREE.InstancedBufferAttribute(new Float32Array(colors), 4);
+    this.geometry.setAttribute('color', colorAttr);
+
+    const orientationStartAttr = new THREE.InstancedBufferAttribute(new Float32Array(orientationsStart), 4); 
+    this.geometry.setAttribute('orientationStart', orientationStartAttr);
+
+    const orientationEndAttr = new THREE.InstancedBufferAttribute(new Float32Array(orientationsEnd), 4); 
+    this.geometry.setAttribute('orientationEnd', orientationEndAttr);
+
+    const material = new THREE.RawShaderMaterial({
+      transparent: true,
+      side: THREE.DoubleSide,
+      uniforms: {
+        'time': { value: 1.0 },
+        'sineTime': { value: 1.0 },
+      },
+      vertexShader: this.vertexShader,
+      fragmentShader: this.fragmentShader,
+    });
+
+    const mesh = new THREE.Mesh(this.geometry, material);
+    mesh.name = "target_mesh";
+    this.scene.add(mesh);
   }
 
   // 创建渲染器
@@ -86,6 +216,20 @@ export class Model {
 
     this.stats?.update();
     this.controls?.update();
+
+    {
+      const time = performance.now();
+
+			const object = this.scene.getObjectByName("target_mesh") as THREE.Mesh;
+      if (object) {
+        object.rotation.y = time * 0.0005;
+
+        const material = object.material as THREE.RawShaderMaterial;
+        material.uniforms['time'].value = time * 0.005;
+        material.uniforms['sineTime'].value = Math.sin(material.uniforms['time'].value * 0.05);
+        object.material = material;
+      }
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
