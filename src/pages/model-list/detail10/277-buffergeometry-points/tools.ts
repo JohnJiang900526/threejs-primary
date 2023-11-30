adf2f93e15e9d4fb45e076fe24af61238046340c
JohnJiang900526 <jiangangshjd@163.com>
2023-11-30 17:31:47 +0800

    完成 277-buffergeometry-points 案例开发和解析

--- src/pages/model-list/detail10/277-buffergeometry-points/tools.ts
+++ src/pages/model-list/detail10/277-buffergeometry-points/tools.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GUI from 'lil-gui';
 
 export class Model {
   private width: number;
@@ -16,6 +16,7 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private points: null | THREE.Points;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,16 +34,22 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.gui.hide();
+    this.points = null;
   }
 
   init() {
     // 场景
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x050505);
+    this.scene.fog = new THREE.Fog(0x050505, 2000, 5000);
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
+    this.camera = new THREE.PerspectiveCamera(80, this.aspect, 5, 5000);
     this.camera.position.z = 2750;
 
+    // 模型
+    this.generateModel();
     // 渲染器
     this.createRenderer();
 
@@ -59,6 +66,51 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  // 创建模型
+  // 核心逻辑
+  private generateModel() {
+    const particles = 5000;
+    const geometry = new THREE.BufferGeometry();
+
+    const positions: number[] = [];
+    const colors: number[] = [];
+    const color = new THREE.Color();
+
+    // 粒子在立方体中扩散
+    const radius = 1000, half = (radius / 2);
+    for (let i = 0; i < particles; i++) {
+      // 位置
+      const x = Math.random() * radius - half;
+      const y = Math.random() * radius - half;
+      const z = Math.random() * radius - half;
+      positions.push(x, y, z);
+
+      // 颜色
+      const r = (x / radius) + 0.5;
+      const g = (y / radius) + 0.5;
+      const b = (z / radius) + 0.5;
+      color.setRGB(r, g, b);
+      colors.push(color.r, color.g, color.b);
+    }
+
+    const positionAttr = new THREE.Float32BufferAttribute(positions, 3);
+    geometry.setAttribute('position', positionAttr);
+
+    const colorAttr = new THREE.Float32BufferAttribute(colors, 3);
+    geometry.setAttribute('color', colorAttr);
+
+    // 计算球形边界
+    geometry.computeBoundingSphere();
+
+    const material = new THREE.PointsMaterial({ 
+      size: 15,
+      // 使用顶点着色
+      vertexColors: true,
+    });
+    this.points = new THREE.Points(geometry, material);
+    this.scene.add(this.points);
   }
 
   // 创建渲染器
@@ -86,6 +138,13 @@ export class Model {
 
     this.stats?.update();
     this.controls?.update();
+
+    {
+      // 控制模型旋转
+      const timer = Date.now() * 0.001 / 2;
+      this.points!.rotation.x = timer * 0.25;
+      this.points!.rotation.y = timer * 0.50;
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
