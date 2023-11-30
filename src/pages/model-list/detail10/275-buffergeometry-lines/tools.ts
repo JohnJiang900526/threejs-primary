1d5724cc17b358eab2d3e297195a1c57e1275fc8
JohnJiang900526 <jiangangshjd@163.com>
2023-11-30 09:56:29 +0800

    完成 275-buffergeometry-lines 案例开发

--- src/pages/model-list/detail10/275-buffergeometry-lines/tools.ts
+++ src/pages/model-list/detail10/275-buffergeometry-lines/tools.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GUI from 'lil-gui';
 
 export class Model {
   private width: number;
@@ -16,6 +16,11 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private clock: THREE.Clock;
+  private line: null | THREE.Line;
+  private segments: number;
+  private radius: number;
+  private timer: number;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,6 +38,12 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.gui.hide();
+    this.clock = new THREE.Clock();
+    this.line = null;
+    this.segments = 10000;
+    this.radius = 800;
+    this.timer = 0;
   }
 
   init() {
@@ -40,9 +51,11 @@ export class Model {
     this.scene = new THREE.Scene();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
+    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 4000);
     this.camera.position.z = 2750;
 
+    // 模型
+    this.generateMesh();
     // 渲染器
     this.createRenderer();
 
@@ -61,12 +74,61 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  // 核心
+  private generateMesh() {
+    const geometry = new THREE.BufferGeometry();
+    const material = new THREE.LineBasicMaterial({ vertexColors: true });
+
+    const positions = [];
+    const colors = [];
+
+    for (let i = 0; i < this.segments; i++) {
+      const x = Math.random() * this.radius - this.radius / 2;
+      const y = Math.random() * this.radius - this.radius / 2;
+      const z = Math.random() * this.radius - this.radius / 2;
+
+      // 位置
+      positions.push(x, y, z);
+
+      // 颜色
+      colors.push((x / this.radius) + 0.5);
+      colors.push((y / this.radius) + 0.5);
+      colors.push((z / this.radius) + 0.5);
+    }
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    this.generateMorphTargets(geometry);
+    geometry.computeBoundingSphere();
+
+    this.line = new THREE.Line(geometry, material);
+    this.scene.add(this.line);
+  }
+
   // 创建渲染器
   private createRenderer() {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
+  }
+
+  // 核心
+  private generateMorphTargets(geometry: THREE.BufferGeometry) {
+    const data: number[] = [];
+
+    for (let i = 0; i < this.segments; i++) {
+      data.push(
+        Math.random() * this.radius - this.radius / 2,
+        Math.random() * this.radius - this.radius / 2,
+        Math.random() * this.radius - this.radius / 2,
+      );
+    }
+
+    const morphTarget = new THREE.Float32BufferAttribute(data, 3);
+    morphTarget.name = 'target1';
+    geometry.morphAttributes.position = [morphTarget];
   }
 
   // 性能统计
@@ -86,6 +148,19 @@ export class Model {
 
     this.stats?.update();
     this.controls?.update();
+
+    {
+      if (this.line) {
+        const delta = this.clock.getDelta();
+        const time = this.clock.getElapsedTime();
+
+        this.line.rotation.x = time * 0.25;
+        this.line.rotation.y = time * 0.5;
+
+        this.timer += delta * 0.5;
+        this.line.morphTargetInfluences![0] = Math.abs(Math.sin(this.timer));
+      }
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
