7ef030142e7142e89f356b96d5b7f63220872e03
JohnJiang900526 <jiangangshjd@163.com>
2023-12-19 16:44:42 +0800

    完成 310-volume-cloud 案例开发

--- src/pages/model-list/detail11/310-volume-cloud/tools.ts
+++ src/pages/model-list/detail11/310-volume-cloud/tools.ts
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GUI from 'lil-gui';
+import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise';
+import WebGL from 'three/examples/jsm/capabilities/WebGL';
+import { showFailToast } from 'vant';
+import { fragmentShader, vertexShader } from './vars';
 
 export class Model {
   private width: number;
@@ -16,6 +20,14 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private mesh: THREE.Mesh;
+  private params: {
+    threshold: number;
+    opacity: number;
+    range: number;
+    steps: number;
+  }
+  private material: THREE.RawShaderMaterial;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,16 +45,33 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.mesh = new THREE.Mesh();
+    this.params = {
+      threshold: 0.25,
+      opacity: 0.25,
+      range: 0.1,
+      steps: 100,
+    };
+    this.material = new THREE.RawShaderMaterial();
   }
 
   init() {
+    if (!WebGL.isWebGL2Available()) {
+      showFailToast(WebGL.getWebGL2ErrorMessage());
+      return false;
+    }
+    
     // 场景
     this.scene = new THREE.Scene();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
-    this.camera.position.z = 2750;
+    this.camera = new THREE.PerspectiveCamera(90, this.aspect, 0.1, 100);
+    this.camera.position.z = 2;
 
+    // 天空
+    this.createSky();
+    // mesh
+    this.createMesh();
     // 渲染器
     this.createRenderer();
 
@@ -50,6 +79,7 @@ export class Model {
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.update();
 
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -59,6 +89,91 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private setGUI() {
+    const update = () => {
+      this.material.uniforms.threshold.value = this.params.threshold;
+      this.material.uniforms.opacity.value = this.params.opacity;
+      this.material.uniforms.range.value = this.params.range;
+      this.material.uniforms.steps.value = this.params.steps;
+    };
+    
+    this.gui.add(this.params, 'threshold', 0, 1, 0.01).onChange(update);
+    this.gui.add(this.params, 'opacity', 0, 1, 0.01).onChange(update);
+    this.gui.add(this.params, 'range', 0, 1, 0.01).onChange(update);
+    this.gui.add(this.params, 'steps', 0, 200, 1).onChange(update);
+  }
+
+  private createMesh() {
+    const size = 128;
+    const data = new Uint8Array(size * size * size);
+
+    let i = 0;
+    const scale = 0.05;
+    const perlin = new ImprovedNoise();
+    const vector = new THREE.Vector3();
+
+    for (let z = 0; z < size; z++) {
+      for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+          const d = 1.0 - vector.set(x, y, z).subScalar(size / 2).divideScalar(size).length();
+          data[i] = (128 + 128 * perlin.noise(x * scale / 1.5, y * scale, z * scale / 1.5)) * d * d;
+          i++;
+        }
+      }
+    }
+
+    const texture = new THREE.Data3DTexture(data, size, size, size);
+    texture.format = THREE.RedFormat;
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.unpackAlignment = 1;
+    texture.needsUpdate = true;
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.RawShaderMaterial({
+      glslVersion: THREE.GLSL3,
+      uniforms: {
+        base: { value: new THREE.Color(0x798aa0) },
+        map: { value: texture },
+        cameraPos: { value: new THREE.Vector3() },
+        threshold: { value: 0.25 },
+        opacity: { value: 0.25 },
+        range: { value: 0.1 },
+        steps: { value: 100 },
+        frame: { value: 0 },
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      side: THREE.BackSide,
+      transparent: true,
+    });
+
+    this.mesh = new THREE.Mesh(geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  private createSky() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 1;
+    canvas.height = 32;
+
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const gradient = context.createLinearGradient(0, 0, 0, 32);
+    gradient.addColorStop(0.0, '#014a84');
+    gradient.addColorStop(0.5, '#0561a0');
+    gradient.addColorStop(1.0, '#437ab6');
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, 1, 32);
+
+    const geometry = new THREE.SphereGeometry(10);
+    const material = new THREE.MeshBasicMaterial({ 
+      map: new THREE.CanvasTexture(canvas), 
+      side: THREE.BackSide,
+    });
+    const sky = new THREE.Mesh(geometry, material);
+    this.scene.add(sky);
   }
 
   // 创建渲染器
@@ -86,6 +201,13 @@ export class Model {
 
     this.stats?.update();
     this.controls?.update();
+
+    {
+      const timer = performance.now();
+      this.material.uniforms.cameraPos.value.copy(this.camera!.position);
+      this.material.uniforms.frame.value++;
+      this.mesh.rotation.y = -timer / 7500;
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
