ff89df550ebbf0116f4a2281abc95f7a456025e5
JohnJiang900526 <jiangangshjd@163.com>
2023-12-20 13:30:16 +0800

    完成 312-volume-perlin 案例开发

--- src/pages/model-list/detail11/312-volume-perlin/tools.ts
+++ src/pages/model-list/detail11/312-volume-perlin/tools.ts
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GUI from 'lil-gui';
+import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise';
+import WebGL from 'three/examples/jsm/capabilities/WebGL';
+import { showFailToast } from 'vant';
+import { fragmentShader, vertexShader } from './vars';
 
 export class Model {
   private width: number;
@@ -16,6 +20,12 @@ export class Model {
 
   private controls: null | OrbitControls;
   private gui: GUI;
+  private mesh: THREE.Mesh;
+  private material: THREE.RawShaderMaterial;
+  private params: { 
+    threshold: number;
+    steps: number;
+  };
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -33,23 +43,34 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
+    this.mesh = new THREE.Mesh();
+    this.material = new THREE.RawShaderMaterial();
+    this.params = { threshold: 0.6, steps: 200 };
   }
 
   init() {
+    if (!WebGL.isWebGL2Available()) {
+      showFailToast(WebGL.getWebGL2ErrorMessage());
+      return false;
+    }
     // 场景
     this.scene = new THREE.Scene();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
-    this.camera.position.z = 2750;
+    this.camera = new THREE.PerspectiveCamera(60, this.aspect, 0.1, 100);
+    this.camera.position.z = 3;
 
+    // mesh
+    this.createMesh();
     // 渲染器
     this.createRenderer();
 
     // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
+    this.controls.enableDamping = true;
     this.controls.update();
 
+    this.setGUI();
     this.initStats();
     this.animate();
     this.resize();
@@ -59,6 +80,59 @@ export class Model {
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
+  }
+
+  private setGUI() {
+    this.gui.add(this.params, 'threshold', 0, 1, 0.01);
+    this.gui.add(this.params, 'steps', 0, 300, 1);
+  }
+
+  private createMesh() {
+    const size = 128;
+    const data = new Uint8Array(size * size * size);
+
+    const perlin = new ImprovedNoise();
+    const vector = new THREE.Vector3();
+    let i = 0;
+
+    for (let z = 0; z < size; z++) {
+      for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+          vector.set(x, y, z).divideScalar(size);
+          const d = perlin.noise(vector.x * 6.5, vector.y * 6.5, vector.z * 6.5);
+          data[i++] = (d * size + size);
+        }
+      }
+    }
+
+    const texture = new THREE.Data3DTexture(data, size, size, size);
+    texture.format = THREE.RedFormat;
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    // 打开对齐方式
+    // 默认为4。指定内存中每个像素行起点的对齐要求。 
+    // 允许的值为1（字节对齐）、2（行对齐到偶数字节）、4（字对齐）和8（行从双字边界开始）。 
+    // 请参阅glPixelStorei来了解详细信息。
+    texture.unpackAlignment = 1;
+    texture.needsUpdate = true;
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    if (this.material) {this.material.dispose()}
+    this.material = new THREE.RawShaderMaterial({
+      uniforms: {
+        map: { value: texture },
+        cameraPos: { value: new THREE.Vector3() },
+        threshold: { value: 0.6 },
+        steps: { value: 200 }
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      side: THREE.BackSide,
+      glslVersion: THREE.GLSL3,
+    });
+
+    this.mesh = new THREE.Mesh(geometry, this.material);
+    this.scene.add(this.mesh);
   }
 
   // 创建渲染器
@@ -87,6 +161,11 @@ export class Model {
     this.stats?.update();
     this.controls?.update();
 
+    this.material.uniforms.cameraPos.value.copy(this.camera!.position);
+    this.material.uniforms.threshold.value = this.params.threshold;
+    this.material.uniforms.steps.value = this.params.steps;
+
+    this.mesh.rotation.y += 0.005;
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
   }
