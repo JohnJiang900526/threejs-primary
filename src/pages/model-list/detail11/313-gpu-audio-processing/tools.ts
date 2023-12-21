d72d615ebaff4cb44eef758729a86b2f3f5bf094
JohnJiang900526 <jiangangshjd@163.com>
2023-12-21 11:46:39 +0800

    完成 313-gpu-audio-processing 案例开发

--- src/pages/model-list/detail11/313-gpu-audio-processing/tools.ts
+++ src/pages/model-list/detail11/313-gpu-audio-processing/tools.ts
@@ -1,7 +1,17 @@
 import * as THREE from 'three';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import GUI from 'lil-gui';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import {
+  ShaderNode, compute,
+  uniform, element, storage, instanceIndex,
+  float, assign, add, sub, div, mul, texture, viewportTopLeft, color
+} from 'three/examples/jsm/nodes/Nodes';
+// @ts-ignore
+import WebGPU from 'three/examples/jsm/capabilities/WebGPU';
+// @ts-ignore
+import WebGPURenderer from 'three/examples/jsm/renderers/webgpu/WebGPURenderer';
+import { showFailToast } from 'vant';
+
 
 export class Model {
   private width: number;
@@ -9,13 +19,25 @@ export class Model {
   private aspect: number;
   private container: HTMLDivElement;
   private scene: THREE.Scene;
-  private renderer: null | THREE.WebGLRenderer;
+  private renderer: null | WebGPURenderer;
   private camera: null | THREE.PerspectiveCamera;
   private stats: null | Stats;
   private animateNumber: number;
 
-  private controls: null | OrbitControls;
   private gui: GUI;
+  private computeNode: any;
+  private waveBuffer: Float32Array;
+  private sampleRate: number;
+  private waveGPUBuffer: THREE.InstancedBufferAttribute;
+  private currentAudio: null | AudioBufferSourceNode;
+  private currentAnalyser: null | AnalyserNode;
+  private analyserBuffer: Uint8Array;
+  private analyserTexture: THREE.DataTexture;
+
+  private pitch: any;
+  private delayVolume: any;
+  private delayOffset: any;
+  private initBgNode: boolean;
   constructor(container: HTMLDivElement) {
     this.container = container;
     this.width = this.container.offsetWidth;
@@ -27,31 +49,44 @@ export class Model {
     this.stats = null;
     this.animateNumber = 0;
 
-    this.controls = null;
     this.gui = new GUI({
       title: "控制面板",
       autoPlace: false,
       container: this.container,
     });
+    this.computeNode = {};
+    this.waveBuffer = new Float32Array();
+    this.sampleRate = 0;
+    this.waveGPUBuffer = new THREE.InstancedBufferAttribute(new Float32Array(), 0);
+    this.currentAudio = null;
+    this.currentAnalyser = null;
+    this.analyserBuffer = new Uint8Array(1024);
+    this.analyserTexture = new THREE.DataTexture();
+
+    this.pitch = uniform(1.5);
+    this.delayVolume = uniform(0.20);
+    this.delayOffset = uniform(0.55);
+    this.initBgNode = false;
   }
 
   init() {
+    if (!WebGPU.isAvailable()) {
+      showFailToast(WebGPU.getErrorMessage());
+      return false;
+    }
+
     // 场景
     this.scene = new THREE.Scene();
+    this.initNodes();
 
     // 相机
-    this.camera = new THREE.PerspectiveCamera(50, this.aspect, 1, 3500);
-    this.camera.position.z = 2750;
+    this.camera = new THREE.PerspectiveCamera(45, this.aspect, 0.01, 30);
 
     // 渲染器
     this.createRenderer();
 
-    // 控制器
-    this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
-    this.controls.update();
-
+    this.setGUI();
     this.initStats();
-    this.animate();
     this.resize();
   }
 
@@ -61,11 +96,108 @@ export class Model {
     return userAgent.includes("mobile");
   }
 
+  startHandle() {
+    this.initBuffer().then(() => {
+      this.playAudio();
+    });
+  }
+
+  private setGUI() {
+    this.gui.add(this.pitch, 'value', 0.5, 2, 0.01).name('pitch');
+    this.gui.add(this.delayVolume, 'value', 0, 1, 0.01).name('delayVolume');
+    this.gui.add(this.delayOffset, 'value', 0.1, 1, 0.01).name('delayOffset');
+
+    this.gui.add(this, "startHandle").name("开始执行");
+  }
+
+  private initNodes() {
+    this.analyserTexture = new THREE.DataTexture(
+      this.analyserBuffer, 
+      this.analyserBuffer.length, 
+      1, THREE.RedFormat,
+    );
+
+    if (this.initBgNode) {
+      const spectrum = mul(texture(this.analyserTexture, viewportTopLeft.x).x, viewportTopLeft.y);
+      const backgroundNode = mul(color(0x0000FF), spectrum);
+      Object.assign(this.scene, { backgroundNode: backgroundNode });
+    }
+  }
+
+  // 核心
+  private async initBuffer() {
+    const url = "/examples/sounds/webgpu-audio-processing.mp3";
+    const soundBuffer = await fetch(url).then((data) => {
+      return data.arrayBuffer();
+    });
+    const audioContext = new AudioContext();
+    const audioBuffer = await audioContext.decodeAudioData(soundBuffer);
+
+    this.waveBuffer = audioBuffer.getChannelData(0);
+    this.waveBuffer = new Float32Array([...this.waveBuffer, ...new Float32Array(200000)]);
+    this.sampleRate = audioBuffer.sampleRate / audioBuffer.numberOfChannels;
+
+    this.waveGPUBuffer = new THREE.InstancedBufferAttribute(this.waveBuffer, 1);
+    // @ts-ignore
+    const waveStorageNode = storage(this.waveGPUBuffer, 'float', this.waveBuffer.length);
+    const tempBuffer = new THREE.InstancedBufferAttribute(this.waveBuffer, 1)
+    // @ts-ignore
+    const waveNode = storage(tempBuffer, 'float', this.waveBuffer.length);
+
+    // @ts-ignore
+    const computeShaderNode = new ShaderNode((inputs, builder) => {
+      const index = float(instanceIndex);
+      const time = mul(index, this.pitch);
+
+      let wave = element(waveNode, time);
+
+      for (let i = 1; i < 7; i++) {
+        const indexNode = mul(sub(index, mul(mul(this.delayOffset, this.sampleRate), i)), this.pitch);
+        const offset = element(waveNode, indexNode);
+        const offsetVolume = mul(offset, div(this.delayVolume, i * i));
+        wave = add(wave, offsetVolume);
+      }
+      // store
+      const waveStorageElementNode = element(waveStorageNode, instanceIndex);
+      assign(waveStorageElementNode, wave).build(builder);
+    });
+
+    // @ts-ignore
+    this.computeNode = compute(computeShaderNode, this.waveBuffer.length);
+  }
+
+  // 核心
+  private async playAudio() {
+    if (this.currentAudio) { 
+      this.currentAudio.stop(); 
+    }
+
+    this.renderer.compute(this.computeNode);
+
+    const waveArray = await this.renderer.getArrayFromBuffer(this.waveGPUBuffer);
+    const audioOutputContext = new AudioContext({sampleRate: this.sampleRate});
+    const audioOutputBuffer = audioOutputContext.createBuffer(1, waveArray.length, this.sampleRate);
+    audioOutputBuffer.copyToChannel(waveArray, 0);
+
+    const source = audioOutputContext.createBufferSource();
+    source.connect(audioOutputContext.destination);
+    source.buffer = audioOutputBuffer;
+    // 开始播放
+    source.start();
+
+    this.currentAudio = source;
+    // 创建分析器
+    this.currentAnalyser = audioOutputContext.createAnalyser();
+    this.currentAnalyser.fftSize = 2048;
+    source.connect(this.currentAnalyser);
+  }
+
   // 创建渲染器
   private createRenderer() {
-    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer = new WebGPURenderer();
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
+    this.renderer.setAnimationLoop(() => { this.render(); });
     this.container.appendChild(this.renderer.domElement);
   }
 
@@ -79,13 +211,14 @@ export class Model {
     this.container.appendChild(this.stats.domElement);
   }
 
-  // 持续动画
-  private animate() {
-    this.animateNumber && window.cancelAnimationFrame(this.animateNumber);
-    this.animateNumber = window.requestAnimationFrame(() => { this.animate(); });
-
+  // 渲染
+  private render() {
     this.stats?.update();
-    this.controls?.update();
+
+    if (this.currentAnalyser) {
+      this.currentAnalyser.getByteFrequencyData(this.analyserBuffer);
+      this.analyserTexture.needsUpdate = true;
+    }
 
     // 执行渲染
     this.renderer?.render(this.scene, this.camera!);
